9d046586bc3551452c5010359834475e4a5acdfe
nyarla <nyarla@kalaclista.com>
2020-04-18 15:04:10 +0900

    update: complete disabled kvm

--- config/per-desktop/virtualbox.nix
+++ config/per-desktop/virtualbox.nix
@@ -1,6 +1,15 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
   virtualisation.virtualbox.host.enableHardening = false;
+
+  boot.blacklistedKernelModules = [
+    "kvm_intel" "kvm" "kvmgt"
+  ];
+
+  boot.extraModprobeConfig = ''
+    install kvm_intel ${pkgs.coreutils}/bin/true
+    install kvm ${pkgs.coreutils}/bin/true
+  '';
 }

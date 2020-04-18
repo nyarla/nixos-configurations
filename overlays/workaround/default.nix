d7a4fa5a09cf83fe05ed896de85ee7205d2b62db
nyarla <nyarla@kalaclista.com>
2020-04-18 15:12:56 +0900

    improved: use latest bitwig

--- overlays/workaround/default.nix
+++ overlays/workaround/default.nix
@@ -1,6 +1,13 @@
 self: super: let
   require = path: args: super.callPackage (import path) args ;
 in { 
-
+  bitwig-studio3 = super.bitwig-studio3.overrideAttrs (old: rec {
+    name = "bitwig-studio3-${version}";
+    version = "3.1.3";
+    src = super.fetchurl {
+      url = "https://downloads.bitwig.com/stable/3.1.3/bitwig-studio-3.1.3.deb";
+      sha256 = "11z5flmp55ywgxyccj3pzhijhaggi42i2pvacg88kcpj0cin57vl";
+    }; 
+  });
 }
   

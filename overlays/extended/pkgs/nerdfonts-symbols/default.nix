412c7ae8de0ddd04bbfb1c872c8734eb871a27fa
nyarla <nyarla@kalaclista.com>
2020-12-22 12:44:27 +0900

    updated: updated sha256 hash for nerdfont

--- overlays/extended/pkgs/nerdfonts-symbols/default.nix
+++ overlays/extended/pkgs/nerdfonts-symbols/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec{
   src = fetchurl {
     name = "nerdfont-symbols.ttf";
     url = "https://github.com/ryanoasis/nerd-fonts/raw/master/src/glyphs/Symbols-2048-em%20Nerd%20Font%20Complete.ttf";
-    sha256 = "078ynwfl92p8pq1n3ic07248whdjm30gcvkq3sy9gas1vlpyg6an";
+    sha256 = "004nlkn834p544rx9jm1zf6ag3zyk3y318ispk4yih07fy7yasyz";
   };
 
   unpackPhase = ''

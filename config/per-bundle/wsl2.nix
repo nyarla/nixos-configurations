65c0209e6a1d913f42c5e504898d36419eecc1bc
nyarla <nyarla@kalaclista.com>
2021-02-15 10:19:42 +0900

    improved: use syncthing on WSL2

--- config/per-bundle/wsl2.nix
+++ config/per-bundle/wsl2.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   imports = [ ../per-desktop/fonts.nix ../per-desktop/uim.nix ];
 
-  environment.systemPackages = with pkgs; [ mlterm xclip ];
+  environment.systemPackages = with pkgs; [ mlterm xclip syncthing ];
 
-  services.dbus.packages = with pkgs; [ mlterm xclip ];
+  services.dbus.packages = with pkgs; [ mlterm xclip syncthing ];
 }

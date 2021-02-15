e95032894924e1f77495671f6e088acba00848d5
nyarla <nyarla@kalaclista.com>
2021-02-15 10:17:12 +0900

    improved: improved systemd bootstrap command

--- config/per-machine/WSL2.nix
+++ config/per-machine/WSL2.nix
@@ -4,6 +4,21 @@ let
   syschdemd = import ../../external/WSL2/syschdemd.nix {
     inherit lib pkgs config defaultUser;
   };
+  systemd-bootstrap = pkgs.stdenv.mkDerivation rec {
+    name = "systemd-bootstrap-${version}";
+    version = "2021-02-15";
+    extraBootstrapConfig = ''
+      ulimit -n 65535
+    '';
+    buildInputs = [ pkgs.gnused syschdemd ];
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/bin/
+      cp ${syschdemd}/bin/syschdemd $out/bin/syschdemd
+      extraConfig="$(echo '${extraBootstrapConfig}' | sed "s/\n/ ; /g")"
+      sed -i "s!exec!''${extraConfig}; exec!" $out/bin/syschdemd
+    '';
+  };
 in {
   boot.isContainer = true;
   boot.cleanTmpDir = true;
@@ -18,7 +33,7 @@ in {
   networking.dhcpcd.enable = false;
 
   users.users.root = {
-    shell = "${syschdemd}/bin/syschdemd";
+    shell = "${systemd-bootstrap}/bin/syschdemd";
     extraGroups = [ "root" ];
   };
 

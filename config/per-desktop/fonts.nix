b46cede31dd87f68e3ae46fdb6fc5bda72700c04
nyarla <nyarla@kalaclista.com>
2021-04-23 10:06:35 +0900

    workaround: temporary disabled noto-fonts-jp

--- config/per-desktop/fonts.nix
+++ config/per-desktop/fonts.nix
@@ -3,13 +3,13 @@
     fonts = with pkgs; [
       # basical fonts
       noto-fonts
-      noto-fonts-jp
+      # noto-fonts-jp
       noto-fonts-extra
       noto-fonts-emoji
 
       # monospace
       myrica
-      (nerdfonts.override { fonts = [ "Noto" ]; })
+      nerdfonts-symbols
 
       # documenation
       genjyuu-gothic
@@ -18,9 +18,9 @@
     fontconfig = {
       enable = true;
       defaultFonts = {
-        sansSerif = [ "Noto Sans JP" ];
-        serif = [ "Noto Serif JP" ];
-        monospace = [ "MyricaM M" ];
+        sansSerif = [ "Noto Sans" ];
+        serif = [ "Noto Serif" ];
+        monospace = [ "MyricaM M" "Noto Color Emoji" "Symbols Nerd Font" ];
         emoji = [ "Noto Color Emoji" ];
       };
       subpixel = { lcdfilter = "light"; };

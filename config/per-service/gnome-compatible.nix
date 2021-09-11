78fcccac984928c9fcbfae7188518055d2e846de
nyarla <nyarla@kalaclista.com>
2021-09-12 07:02:07 +0900

    update: use services improvement

--- config/per-service/gnome-compatible.nix
+++ config/per-service/gnome-compatible.nix
@@ -3,12 +3,14 @@ let apps = with pkgs.gnome3; [ zenity gsound dconf dconf-editor ];
 in {
   environment.systemPackages = apps ++ (with pkgs; [ gtk3 ]);
   services.dbus.packages = apps;
+  services.dbus.enable = true;
 
   programs.dconf.enable = true;
 
   services.accounts-daemon.enable = true;
   services.gnome.gnome-keyring.enable = true;
   services.gnome.gnome-settings-daemon.enable = true;
+  services.gnome.glib-networking.enable = true;
   services.system-config-printer.enable = true;
   services.upower.enable = config.powerManagement.enable;
 }

f1cf0b09e5875187bf03d64434ca072bc6bb4912
nyarla <nyarla@kalaclista.com>
2021-09-12 06:56:59 +0900

    new: use fbterm as recovery console

--- config/per-desktop/terminal.nix
+++ config/per-desktop/terminal.nix
@@ -1,6 +1,13 @@
 { config, pkgs, ... }:
-let apps = with pkgs; [ mlterm ];
+let apps = with pkgs; [ mlterm fbterm ];
 in {
   environment.systemPackages = apps;
   services.dbus.packages = apps;
+
+  security.wrappers = {
+    fbterm = {
+      source = "${pkgs.fbterm}/bin/fbterm";
+      capabilities = "cap_sys_tty_config+ep";
+    };
+  };
 }

7dd1522424b3f099ec8c58304e623b817074613e
nyarla <nyarla@kalaclista.com>
2021-09-12 07:00:08 +0900

    fix: improvement anti-tearing configuration

--- config/per-hardware/nvidia.nix
+++ config/per-hardware/nvidia.nix
@@ -3,7 +3,7 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
   services.xserver.screenSection = ''
-    Option "metamodes" "nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
+    Option "MetaModes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
     Option "AllowIndirectGLXProtocol" "off"
     Option "TripleBuffer" "on"
   '';

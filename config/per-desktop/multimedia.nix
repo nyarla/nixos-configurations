7edc784be86a0635bf74e900aaab347fab5cc0f6
nyarla <nyarla@kalaclista.com>
2021-09-30 12:01:56 +0900

    update: add audacity

--- config/per-desktop/multimedia.nix
+++ config/per-desktop/multimedia.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }:
-let apps = with pkgs; [ calibre quodlibet-full picard glib.out ];
+let apps = with pkgs; [ calibre quodlibet-full picard glib.out audacity ];
 in {
   environment.systemPackages = apps;
   services.dbus.packages = apps;

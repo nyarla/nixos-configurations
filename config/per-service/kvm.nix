995bd779514b79025d3b45bb63b9134233a4cd1c
nyarla <nyarla@kalaclista.com>
2021-09-30 12:13:36 +0900

    update: qemu run as user

--- config/per-service/kvm.nix
+++ config/per-service/kvm.nix
@@ -1,12 +1,16 @@
 { config, pkgs, ... }:
 let
   gui = if config.services.xserver.enable then
-    (with pkgs; [ virt-manager ])
+    (with pkgs; [ virt-manager remmina ])
   else
     [ ];
 in {
   environment.systemPackages = gui;
   services.dbus.packages = gui;
+
+  boot.kernelModules = [ "amd_iommnu" "pcie_aspm" "iommu" "vfio_pci" ];
+  boot.kernelParams =
+    [ "amd_iommu=on" "pcie_aspm=off" "iommu=pt" "vfio-pci.ids=1022:149c" ];
 
   virtualisation.libvirtd = {
     enable = true;
@@ -17,9 +21,13 @@ in {
       unix_sock_ro_perms = "0770"
       unix_sock_rw_perms = "0770"
       auth_unix_ro = "none"
-      uth_unix_rw = "none"
+      auth_unix_rw = "none"
     '';
-    onBoot = "start";
+    onBoot = "ignore";
     onShutdown = "suspend";
+    qemuVerbatimConfig = ''
+      user = "nyarla"
+      group = "libvirtd"
+    '';
   };
 }

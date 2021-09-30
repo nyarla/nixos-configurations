633ccba53f1219be9a16196e50f3e3a2b6f163ef
nyarla <nyarla@kalaclista.com>
2021-09-30 12:04:35 +0900

    update: add capability to fbterm

--- config/per-desktop/terminal.nix
+++ config/per-desktop/terminal.nix
@@ -6,6 +6,8 @@ in {
 
   security.wrappers = {
     fbterm = {
+      owner = "root";
+      group = "wheel";
       source = "${pkgs.fbterm}/bin/fbterm";
       capabilities = "cap_sys_tty_config+ep";
     };

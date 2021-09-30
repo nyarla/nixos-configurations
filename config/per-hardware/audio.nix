7184cf0723631e8fd19734445da6d7f302d8c76a
nyarla <nyarla@kalaclista.com>
2021-09-30 12:03:54 +0900

    update: update pulseaudio configuration

--- config/per-hardware/audio.nix
+++ config/per-hardware/audio.nix
@@ -14,15 +14,23 @@ in {
     package = pkgs.pulseaudioFull;
     extraModules = [ pkgs.pulseaudio-modules-bt ];
     daemon.config = {
-      default-sample-format = "s24le";
-      default-sample-rate = 192000;
-      alternate-sample-rate = 176000;
-      default-sample-channels = 2;
+      avoid-resampling = "yes";
+      alternate-sample-rate = 192000;
       default-channel-map = "front-left,front-right";
-      resample-method = "speex-float-10";
-      enable-lfe-remixing = "no";
-      high-priority = "yes";
+      default-fragments = 2;
+      default-sample-channels = 2;
+      default-sample-format = "s32le";
+      default-sample-rate = 192000;
       flat-volumes = "no";
+      high-priority = "yes";
+      resample-method = "soxr-vhq";
     };
+    extraConfig = ''
+      load-module module-null-sink sink_name=44100Hz sink_properties=device.description=44100Hz format=s16le rate=44100 channels=2 formats=pcm
+      load-module module-null-sink sink_name=96000Hz sink_properties=device.description=96000Hz format=s24le rate=96000 channels=2 formats=pcm
+
+      load-module module-loopback source=44100Hz format=s16le rate=44100 channels=2 source_dont_move=true adjust_time=0 latency_msec=512
+      load-module module-loopback source=96000Hz format=s24le rate=96000 channels=2 source_dont_move=true adjust_time=0 latency_msec=512
+    '';
   };
 }

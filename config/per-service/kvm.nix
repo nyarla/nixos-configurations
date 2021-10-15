764adefc55060f76d64ad42add15a56d8912dc03
nyarla <nyarla@kalaclista.com>
2021-10-15 11:44:00 +0900

    improvement: add supports for nvidia with rootless qemu, and prepare Single GPU Passthrough

--- config/per-service/kvm.nix
+++ config/per-service/kvm.nix
@@ -4,13 +4,41 @@ let
     (with pkgs; [ virt-manager remmina ])
   else
     [ ];
+
+  qemu_hook = pkgs.runCommand "qemu.sh" { } ''
+    mkdir -p $out/bin
+    cp ${
+      pkgs.fetchurl {
+        url =
+          "https://raw.githubusercontent.com/PassthroughPOST/VFIO-Tools/master/libvirt_hooks/qemu";
+        sha256 = "155dz6535lxxy1pc8yq8kbmc80fh6x35459n7ppf3zwmcdb63rg6";
+      }
+    } $out/bin/qemu
+
+    sed -i 's|#!/bin/bash|#!/run/current-system/sw/bin/bash|' $out/bin/qemu
+    chmod +x $out/bin/qemu
+  '';
 in {
-  environment.systemPackages = gui;
-  services.dbus.packages = gui;
+  environment.systemPackages = gui ++ [ pkgs.bash ];
+  services.dbus.packages = gui ++ [ pkgs.bash ];
 
   boot.kernelModules = [ "amd_iommnu" "pcie_aspm" "iommu" "vfio_pci" ];
-  boot.kernelParams =
-    [ "amd_iommu=on" "pcie_aspm=off" "iommu=pt" "vfio-pci.ids=1022:149c" ];
+  boot.kernelParams = [
+    "kvm.ignore_msrs=1"
+    "amd_iommu=on"
+    "pcie_aspm=off"
+    "iommu=pt"
+    "vfio-pci.ids=1022:149c"
+  ];
+
+  system.activationScripts.libvirt-hooks.text = ''
+    ln -Tfs /etc/libvirt/hooks /var/lib/libvirt/hooks
+  '';
+
+  environment.etc."libvirt/hooks/qemu" = {
+    source = "${qemu_hook}/bin/qemu";
+    mode = "0755";
+  };
 
   virtualisation.libvirtd = {
     enable = true;
@@ -22,12 +50,34 @@ in {
       unix_sock_rw_perms = "0770"
       auth_unix_ro = "none"
       auth_unix_rw = "none"
+      clear_emulator_capabilities = 0
     '';
     onBoot = "ignore";
-    onShutdown = "suspend";
+    onShutdown = "shutdown";
     qemuVerbatimConfig = ''
       user = "nyarla"
       group = "libvirtd"
+
+      cgroup_device_acl = [
+        "/dev/null",
+        "/dev/full",
+        "/dev/zero",
+        "/dev/random",
+        "/dev/urandom",
+        "/dev/ptmx",
+        "/dev/kvm",
+
+        "/dev/nvidia-modeset",
+        "/dev/nvidia-uvm",
+        "/dev/nvidia-uvm-tools",
+        "/dev/nvidia0",
+        "/dev/nvidia1",
+        "/dev/nvidia2",
+        "/dev/nvidia3",
+        "/dev/nvidiactl",
+        "/dev/dri/card0",
+        "/dev/dri/renderD128"
+      ]
     '';
   };
 }

c2eb66dfa9dbb51216218cb150fbb03def130ceb
nyarla <nyarla@kalaclista.com>
2021-10-22 10:08:26 +0900

    update: add some virtual streams to pulseaudio

--- config/per-hardware/audio.nix
+++ config/per-hardware/audio.nix
@@ -29,8 +29,8 @@ in {
       load-module module-null-sink sink_name=44100Hz sink_properties=device.description=44100Hz format=s16le rate=44100 channels=2 formats=pcm
       load-module module-null-sink sink_name=96000Hz sink_properties=device.description=96000Hz format=s24le rate=96000 channels=2 formats=pcm
 
-      load-module module-loopback source=44100Hz format=s16le rate=44100 channels=2 source_dont_move=true adjust_time=0 latency_msec=512
-      load-module module-loopback source=96000Hz format=s24le rate=96000 channels=2 source_dont_move=true adjust_time=0 latency_msec=512
+      load-module module-loopback source=44100Hz source_dont_move=true adjust_time=0 latency_msec=1 remix=false
+      load-module module-loopback source=96000Hz source_dont_move=true adjust_time=0 latency_msec=1 remix=false
     '';
   };
 }

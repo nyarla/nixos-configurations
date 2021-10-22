5fb832f0cacd7bcf6f3c08d4a606d9266e41df94
nyarla <nyarla@kalaclista.com>
2021-10-22 10:11:55 +0900

    update: use auto login as nyarla

--- config/per-service/xorg.nix
+++ config/per-service/xorg.nix
@@ -13,43 +13,9 @@ in {
 
     displayManager = {
       job.environment.LANG = "ja_JP.UTF-8";
-      lightdm = {
+      autoLogin = {
         enable = true;
-        greeters = {
-          mini = {
-            enable = true;
-            user = "nyarla";
-            extraConfig = ''
-              [greeter]
-              show-password-label   = true
-              show-input-cursor     = true
-              password-label-text   = login:
-
-              [greeter-hotkeys]
-              mod-key       = control
-              shutdown-key  = s
-              suspend-key   = p
-              hibernate-key = h
-              restart-key   = r
-
-              [greeter-theme]
-              font              = "Sans"
-              font-size         = 1em
-              text-color        = "#F9F9F9"
-              error-color       = "#FF0000"
-              background-image  = ""
-              background-color  = "#333333"
-              window-color      = "#333333"
-              border-color      = "#00CCFF"
-              border-width      = 1px
-              password-border-color = "#00CCFF"
-              password-border-width = 1px
-              layout-space      = 30
-              password-color            = "#FFFFFF"
-              password-background-color = "#333333"
-            '';
-          };
-        };
+        user = "nyarla";
       };
     };
   };

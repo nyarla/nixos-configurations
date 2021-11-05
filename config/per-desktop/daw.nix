75925528ca56f93e9ad89dab22eb6a2cedffa242
nyarla <nyarla@kalaclista.com>
2021-11-05 19:40:17 +0900

    new: add daw related packages

---
+++ config/per-desktop/daw.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+let
+  packages = with pkgs;
+    [ a2jmidid jack2Full qjackctl ] ++ [ bitwig-studio3 carla ] ++ [
+      adlplug
+      ams-lv2
+      bristol
+      calf
+      # dexed
+      drumgizmo
+      drumkv1
+      fluidsynth
+      fmsynth
+      helm
+      hydrogen
+      linuxsampler
+      # surge-synthesizer
+      synthv1
+      yoshimi
+    ];
+in {
+  boot.kernelModules = [ "snd-seq" "snd-rawmidi" ];
+  boot.kernel.sysctl = {
+    "vm.swapiness" = 10;
+    "fs.inotify.max_user_watches" = 524288;
+  };
+  boot.kernelParams = [ "threadirq" ];
+  security.pam.loginLimits = [
+    {
+      domain = "@audio";
+      item = "memlock";
+      type = "-";
+      value = "unlimited";
+    }
+    {
+      domain = "@audio";
+      item = "rtprio";
+      type = "-";
+      value = "99";
+    }
+    {
+      domain = "@audio";
+      item = "nofile";
+      type = "soft";
+      value = "99999";
+    }
+    {
+      domain = "@audio";
+      item = "nofile";
+      type = "hard";
+      value = "99999";
+    }
+  ];
+
+  environment.systemPackages = packages;
+  services.dbus.packages = packages;
+}

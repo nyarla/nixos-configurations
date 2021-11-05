5b7fde235bb8d8a0244ac2d45b9ca074442f277e
nyarla <nyarla@kalaclista.com>
2021-11-05 19:38:44 +0900

    update: add clipboard keeper script

--- config/per-service/xorg.nix
+++ config/per-service/xorg.nix
@@ -6,6 +6,32 @@ in {
 
   console.useXkbConfig = true;
 
+  systemd.user.services.clipboard-keep = {
+    enable = true;
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    path = [ pkgs.xclip ];
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+      ExecStart = "${pkgs.writeShellScript "clipboard-keep.sh" ''
+        clipboard=
+
+        while true; do
+          content="$(${pkgs.xclip}/bin/xclip -o -selection clipboard 2>/dev/null)"
+
+          if test -z "''${content:-}"; then
+            echo -n $clipboard | ${pkgs.xclip}/bin/xclip -i -selection clipboard -rmlastnl
+          else
+            export clipboard="''${content:-}"
+          fi
+
+          sleep 1
+        done
+      ''}";
+    };
+  };
+
   services.xserver = {
     enable = true;
     autorun = true;

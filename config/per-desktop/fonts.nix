6fd56c5c432d3748ddce00ace0f718d5d5b1b0c0
nyarla <nyarla@kalaclista.com>
2021-12-20 09:47:35 +0900

    update: use Twitter Color Eomoji && use DejaVu font as fallback font

--- config/per-desktop/fonts.nix
+++ config/per-desktop/fonts.nix
@@ -5,27 +5,45 @@
       noto-fonts
       noto-fonts-jp
       noto-fonts-extra
-      noto-fonts-emoji
+      twemoji-color-font
 
       # monospace
       myrica
       nerdfont-symbols-2048
+
+      # fallback
+      dejavu_fonts
     ];
 
     fontconfig = {
       enable = true;
+
+      localConf = ''
+        <?xml version='1.0'?>
+        <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+        <fontconfig>
+          <selectfont>
+            <rejectfont>
+              <pattern>
+                <patelt name="family">
+                  <string>Noto Color Emoji</string>
+                </patelt>
+              </pattern>
+              <pattern>
+                <patelt name="family">
+                  <string>Noto Emoji</string>
+                </patelt>
+              </pattern>
+            </rejectfont>
+          </selectfont>
+        </fontconfig>
+      '';
+
       defaultFonts = {
-        sansSerif =
-          [ "Noto Sans CJK JP" "Noto Sans Symbols" "Noto Sans Symbols2" ];
-        serif = [ "Noto Serif JP" "Noto Sans Symbols" "Noto Sans Symbols2" ];
-        monospace = [
-          "MyricaM M"
-          "Symbols Nerd Font"
-          "Noto Color Emoji"
-          "Noto Sans Symbols"
-          "Noto Sans Symbols2"
-        ];
-        emoji = [ "Noto Color Emoji" ];
+        sansSerif = [ "Noto Sans CJK JP" "DejaVu Sans" ];
+        serif = [ "Noto Serif JP" "DejaVu Serif" ];
+        monospace = [ "MyricaM M" "Symbols Nerd Font" "DejaVu Sans" ];
+        emoji = [ "Twitter Color Emoji" ];
       };
       subpixel = { lcdfilter = "light"; };
     };

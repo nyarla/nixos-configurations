b6f08c723e2982dd1ab6161035513dc597f27f70
nyarla <nyarla@kalaclista.com>
2021-12-20 09:51:10 +0900

    update: disable display-manager

--- config/per-service/xorg.nix
+++ config/per-service/xorg.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }:
-let utils = with pkgs; [ xclip xdg_utils libnotify gksu ];
+let utils = with pkgs; [ xclip xdg_utils libnotify gksu sx ];
 in {
   environment.systemPackages = utils;
   console.useXkbConfig = true;
@@ -30,14 +30,53 @@ in {
     };
   };
 
+  systemd.services.displayManagerCompat = {
+    enable = false;
+    wants = [ "accounts-daemon.service" ];
+    requires = [ "user.slice" ];
+    after = [
+      "rc-local.service"
+      "systemd-machined.service"
+      "systemd-user-sessions.service"
+      "user.slice"
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.coreutils}/bin/true";
+      BusName = "org.freedesktop.DisplayManager";
+      IgnoreSIGPIPE = "no";
+      KeyringMode = "shared";
+      KillMode = "mixed";
+      StandardError = "inherit";
+    };
+  };
+
   services.xserver = {
     enable = true;
-    autorun = true;
+    autorun = false;
     libinput.enable = true;
+    exportConfiguration = true;
 
     displayManager = {
       job.environment.LANG = "ja_JP.UTF-8";
-      lightdm = { enable = true; };
+      lightdm = { enable = false; };
     };
+  };
+
+  security.wrappers = {
+    "Xorg" = {
+      setuid = true;
+      owner = "root";
+      group = "root";
+      source = "${pkgs.xorg.xorgserver}/bin/Xorg";
+    };
+  };
+
+  nixpkgs.config.packageOverrides = super: rec {
+    sx = super.sx.overrideAttrs (old: rec {
+      postPatch = ''
+        sed -i 's!Xorg!/run/wrappers/bin/Xorg!' sx
+      '';
+    });
   };
 }

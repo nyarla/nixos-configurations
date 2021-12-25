2b82e9858480db55fbf00ae1a9968efab598e245
nyarla <nyarla@kalaclista.com>
2021-12-25 16:19:10 +0900

    update: configuraiton improvement for nvidia's GBM support

--- config/per-hardware/nvidia.nix
+++ config/per-hardware/nvidia.nix
@@ -2,23 +2,27 @@
   boot.blacklistedKernelModules = [ "i2c_nvidia_gpu" ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.deviceSection = ''
+    Option "Coolbits" "28"
+    Option "AllowEmptyInitialConfiguration"
+  '';
   services.xserver.screenSection = ''
     Option "MetaModes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
     Option "AllowIndirectGLXProtocol" "off"
     Option "TripleBuffer" "on"
-    Option "Coolbits" "28"
-    Option "AllowEmptyInitialConfiguration"
   '';
 
-  hardware.nvidia.modesetting.enable = false;
+  hardware.nvidia.modesetting.enable = true;
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
     setLdLibraryPath = true;
-    extraPackages = with pkgs; [ libglvnd libvdpau-va-gl ];
+    extraPackages = with pkgs; [ libglvnd libvdpau-va-gl mesa_drivers ];
   };
 
   environment.etc."glvnd/egl_vendor.d".source =
     "${config.boot.kernelPackages.nvidia_x11}/share/glvnd/egl_vendor.d/";
+  environment.etc."gbm/nvidia-drm_gbm.so".source =
+    "${config.boot.kernelPackages.nvidia_x11}/lib/libnvidia-allocator.so";
 }

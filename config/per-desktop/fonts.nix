6931c1929c18265700dda9a07ae0440c0ab0460d
nyarla <nyarla@kalaclista.com>
2022-01-26 16:36:23 +0900

    feat(fonts): use Noto Color Emoji

--- config/per-desktop/fonts.nix
+++ config/per-desktop/fonts.nix
@@ -5,7 +5,7 @@
       noto-fonts
       noto-fonts-jp
       noto-fonts-extra
-      twemoji-color-font
+      noto-fonts-emoji
 
       # monospace
       myrica
@@ -18,32 +18,11 @@
     fontconfig = {
       enable = true;
 
-      localConf = ''
-        <?xml version='1.0'?>
-        <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
-        <fontconfig>
-          <selectfont>
-            <rejectfont>
-              <pattern>
-                <patelt name="family">
-                  <string>Noto Color Emoji</string>
-                </patelt>
-              </pattern>
-              <pattern>
-                <patelt name="family">
-                  <string>Noto Emoji</string>
-                </patelt>
-              </pattern>
-            </rejectfont>
-          </selectfont>
-        </fontconfig>
-      '';
-
       defaultFonts = {
         sansSerif = [ "Noto Sans CJK JP" "DejaVu Sans" ];
         serif = [ "Noto Serif JP" "DejaVu Serif" ];
         monospace = [ "MyricaM M" "Symbols Nerd Font" "DejaVu Sans" ];
-        emoji = [ "Twitter Color Emoji" ];
+        emoji = [ "Noto Color Emoji" ];
       };
       subpixel = { lcdfilter = "light"; };
     };

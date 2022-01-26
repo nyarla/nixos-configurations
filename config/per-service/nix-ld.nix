f8ec4a984c33174195de43c5f5aeee6c62e92087
nyarla <nyarla@kalaclista.com>
2022-01-26 16:38:55 +0900

    feat(nix-ld): add zlib to nix-ld

--- config/per-service/nix-ld.nix
+++ config/per-service/nix-ld.nix
@@ -1,4 +1,5 @@
 { config, pkgs, lib, ... }: {
+  environment.systemPackages = with pkgs; [ zlib ];
   environment.variables = {
     NIX_LD_LIBRARY_PATH = lib.makeLibraryPath config.environment.systemPackages;
     NIX_LD = "${pkgs.stdenv.cc}/nix-support/dynamic-linker";

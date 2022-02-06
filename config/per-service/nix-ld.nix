5e1fee7fb13e3b5b3295b94be0be6d640ec6210d
nyarla <nyarla@kalaclista.com>
2022-02-06 19:45:52 +0900

    chore(nix-ld): cleanup

--- config/per-service/nix-ld.nix
+++ config/per-service/nix-ld.nix
@@ -1,13 +1,11 @@
 { config, pkgs, lib, ... }: {
-  environment.systemPackages = with pkgs; [ zlib stdenv.cc stdenv.cc.cc.lib ];
-  environment.etc."profile.d/nix-ld" = {
-    text = ''
-      export NIX_LD_LIBRARY_PATH="${
-        lib.makeLibraryPath config.environment.systemPackages
-      }"
+  environment.variables = {
+    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+      stdenv.cc.cc.lib
+      stdenv.cc.libc
 
-      export NIX_LD_LIBRARY_PATH=$( echo $NIX_LD_LIBRARY_PATH | tr ':' "\n" | sort | uniq | tr "\n" ":" | sed 's/:$//' )
-      export NIX_LD="$(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker)"
-    '';
+      gtk2
+    ]);
+    NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
   };
 }

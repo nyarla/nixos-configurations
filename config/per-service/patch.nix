1cca78815077ea9b048c77d3be1ff6ed8bfa6765
nyarla <nyarla@kalaclista.com>
2022-02-12 19:40:33 +0900

    chore(flake): update system

--- config/per-service/patch.nix
+++ config/per-service/patch.nix
@@ -1,14 +1,11 @@
 { pkgs, ... }: {
-  system.replaceRuntimeDependencies = [{
-    original = pkgs.alsaLib;
-    replacement = pkgs.alsaLib.overrideAttrs (old: rec {
-      patches = [
-        (pkgs.fetchpatch {
-          url =
-            "https://raw.githubusercontent.com/NixOS/nixpkgs/staging/pkgs/os-specific/linux/alsa-project/alsa-lib/alsa-plugin-conf-multilib.patch";
-          sha256 = "0wj86fcvm50699gjgmcdgfh2zvv3gk25xgyyq9xb3lk9fh8gipy2";
-        })
-      ];
-    });
-  }];
+  nixpkgs.overlays = [
+    (self: super: rec {
+      spice-gtk = super.callPackage (import "${super.fetchurl {
+        url =
+          "https://raw.githubusercontent.com/NixOS/nixpkgs/5c5f2b547f20b7801231253f070a93e1ed9a8546/pkgs/development/libraries/spice-gtk/default.nix";
+        sha256 = "02q4phsmwf540dq4h7n43whc1cmf1kv0g6qpv20yhcc714k4mwpc";
+      }}") { };
+    })
+  ];
 }

5ed1501f844c5d488c6be7b41904574f9e507468
nyarla <nyarla@kalaclista.com>
2022-02-26 12:44:06 +0900

    chore(patch): update nitter revision

--- config/per-service/patch.nix
+++ config/per-service/patch.nix
@@ -5,15 +5,10 @@
         src = super.fetchFromGitHub {
           owner = "zedeus";
           repo = "nitter";
-          rev = "6695784050605c77a301c0a66764fa9a9580a2f5";
-          sha256 = "sha256-1UyObc5oxOSHimcrppzlC/cegrisGLLnLHyRU437rdE=";
+          rev = "ecb6fe4162142f7260b36d4ac4021d6ac7cd2b49";
+          sha256 = "12rd5zhxzrzxgd5zg5kwkdqzzd48g840kf8x55x2a66a3afy6h87";
         };
       });
-      spice-gtk = super.callPackage (import "${super.fetchurl {
-        url =
-          "https://raw.githubusercontent.com/NixOS/nixpkgs/5c5f2b547f20b7801231253f070a93e1ed9a8546/pkgs/development/libraries/spice-gtk/default.nix";
-        sha256 = "02q4phsmwf540dq4h7n43whc1cmf1kv0g6qpv20yhcc714k4mwpc";
-      }}") { };
     })
   ];
 }

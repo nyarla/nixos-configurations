4e546c674c1c12d2aeba0692b60b7a712a415ed5
nyarla <nyarla@kalaclista.com>
2022-03-24 18:44:16 +0900

    chore(patch): temporary fix nitter bug

--- config/per-service/patch.nix
+++ config/per-service/patch.nix
@@ -1,14 +1,20 @@
 { pkgs, lib, ... }: {
   nixpkgs.overlays = [
-    (self: super: rec {
-      nitter = super.nitter.overrideAttrs (old: rec {
-        src = super.fetchFromGitHub {
-          owner = "zedeus";
-          repo = "nitter";
-          rev = "ecb6fe4162142f7260b36d4ac4021d6ac7cd2b49";
-          sha256 = "12rd5zhxzrzxgd5zg5kwkdqzzd48g840kf8x55x2a66a3afy6h87";
-        };
-      });
+    (final: parent: rec {
+      buildPackages_1_6_2 = parent.buildPackages.overrideScope'
+        (super: self: rec {
+          nim-unwrapped = super.nim-unwrapped.overrideAttrs (old: rec {
+            version = "1.6.2";
+            src = super.fetchurl {
+              url = "https://nim-lang.org/download/nim-${version}.tar.xz";
+              hash = "sha256-msRxT6bDFdaR2n9diUHBsZDU1Dc5fZdC4yfC1RiT43M=";
+            };
+          });
+        });
+      nimPackages_1_6_2 = parent.nimPackages.overrideScope'
+        (super: self: rec { buildPackages = final.buildPackages_1_6_2; });
+      nitter =
+        parent.nitter.override { nimPackages = final.nimPackages_1_6_2; };
     })
   ];
 }

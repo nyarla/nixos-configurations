c61b4b230b062fad88bbb0b6488947735dde2111
nyarla <nyarla@kalaclista.com>
2022-04-15 11:31:51 +0900

    chore(network-manager): use latest nixpkgs

--- config/per-service/network-manager.nix
+++ config/per-service/network-manager.nix
@@ -2,7 +2,7 @@
   environment.systemPackages = with pkgs; [ networkmanagerapplet ];
   networking.networkmanager = {
     enable = true;
-    packages = with pkgs; [ networkmanager-openvpn ];
+    plugins = with pkgs; [ networkmanager-openvpn ];
     wifi = {
       backend = "iwd";
       powersave = false;

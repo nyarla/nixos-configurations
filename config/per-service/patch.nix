0b9f42020d6c48ed33c7bb3dfc76497341e17bee
nyarla <nyarla@kalaclista.com>
2022-06-08 11:42:05 +0900

    chore(patch): no use patches at now

--- config/per-service/patch.nix
+++ config/per-service/patch.nix
@@ -1,20 +1,5 @@
-{ pkgs, lib, ... }: {
+_: {
   nixpkgs.overlays = [
-    (final: parent: rec {
-      buildPackages_1_6_2 = parent.buildPackages.overrideScope'
-        (super: self: rec {
-          nim-unwrapped = super.nim-unwrapped.overrideAttrs (old: rec {
-            version = "1.6.2";
-            src = super.fetchurl {
-              url = "https://nim-lang.org/download/nim-${version}.tar.xz";
-              hash = "sha256-msRxT6bDFdaR2n9diUHBsZDU1Dc5fZdC4yfC1RiT43M=";
-            };
-          });
-        });
-      nimPackages_1_6_2 = parent.nimPackages.overrideScope'
-        (super: self: rec { buildPackages = final.buildPackages_1_6_2; });
-      nitter =
-        parent.nitter.override { nimPackages = final.nimPackages_1_6_2; };
-    })
+
   ];
 }

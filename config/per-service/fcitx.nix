39ac0f50d451ea7c4ef01e15decf531d66fa7683
nyarla <nyarla@kalaclista.com>
2022-06-22 17:08:25 +0900

    chore(fcitx5): forgot commit of fcitx.nix file

--- config/per-service/fcitx.nix
+++ config/per-service/fcitx.nix
@@ -1,16 +1,14 @@
 { config, pkgs, ... }: {
   i18n.inputMethod = {
-    enabled = "fcitx";
-    fcitx.engines = with pkgs.fcitx-engines; [ skk ];
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-configtool
+      fcitx5-gtk
+      fcitx5-skk
+      libsForQt5.fcitx5-qt
+    ];
   };
 
   services.dbus.packages = [ config.i18n.inputMethod.package ];
-
   environment.systemPackages = with pkgs; [ skk-dicts skktools ];
-
-  environment.variables = {
-    "GTK_IM_MODULE" = "fcitx";
-    "QT_IM_MODULE" = "fcitx";
-    "XMODIFIERS" = "@im=fcitx";
-  };
 }

3387aa0fa3452834eb83ec66d8cf56ca14048809
nyarla <nyarla@kalaclista.com>
2022-07-07 17:23:07 +0900

    wip(nix-ld): migrate nix-ld from per-service to system/config/nixos

---
+++ system/config/nixos/nix-ld.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, lib, ... }: {
+  environment.variables = {
+    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath
+      (with pkgs; [ cudaPackages.cudatoolkit stdenv.cc.cc.lib stdenv.cc.libc ]);
+  };
+
+  environment.etc."profile.d/nix-ld.sh" = {
+    text = ''
+      export NIX_LD=$(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker)
+    '';
+  };
+}

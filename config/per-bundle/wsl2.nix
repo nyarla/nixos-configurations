40c0fed1f1406d85fb4d72b550871edd34c1f4d8
nyarla <nyarla@kalaclista.com>
2022-07-07 13:25:03 +0900

    chore(trash): remove old configurations

--- config/per-bundle/wsl2.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }: {
-  imports = [ ../per-desktop/fonts.nix ../per-desktop/uim.nix ];
-
-  environment.systemPackages = with pkgs; [ mlterm xclip syncthing ];
-
-  services.dbus.packages = with pkgs; [ mlterm xclip syncthing ];
-}

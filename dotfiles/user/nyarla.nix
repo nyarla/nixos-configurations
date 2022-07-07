b2d5f489eff51cedaa93fd7a7271eb866aec3c5f
nyarla <nyarla@kalaclista.com>
2022-07-07 15:33:56 +0900

    feat(nyarla): add appimage-run

--- dotfiles/user/nyarla.nix
+++ dotfiles/user/nyarla.nix
@@ -1,5 +1,9 @@
 _: {
-  imports = [ ../config/tools/archiver.nix ../config/tools/git.nix ];
+  imports = [
+    ../config/app/appimage.nix
+    ../config/tools/archiver.nix
+    ../config/tools/git.nix
+  ];
 
   home.stateVersion =
     (import ../../system/config/nixos/version.nix).stateVersion;

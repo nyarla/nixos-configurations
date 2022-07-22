3f0ad23fedcb37ec54cc850897357a12c210b0af
nyarla <nyarla@kalaclista.com>
2022-07-22 11:46:57 +0900

    chore(printer): add workaround for GET_PROTOCOL related error

--- pkgs/temporary.nix
+++ pkgs/temporary.nix
@@ -4,6 +4,9 @@ in {
   quodlibet =
     super.quodlibet.overrideAttrs (_: rec { doInstallCheck = false; });
 
+  cnijfilter2 = super.cnijfilter2.overrideAttrs
+    (old: rec { NIX_CFLAGS_COMPILE = " -fcommon"; });
+
   whipper = super.whipper.overrideAttrs (old: rec {
     postPatch = ''
       sed -i 's|cd-paranoia|${super.cdparanoia}/bin/cdparanoia|g' whipper/program/cdparanoia.py

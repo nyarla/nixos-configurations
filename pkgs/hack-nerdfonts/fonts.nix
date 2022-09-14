2ed92d029357d83703d1f48ab505e27290861e93
nyarla <nyarla@kalaclista.com>
2022-09-14 17:37:18 +0900

    chore: update hack-nerdfonts package

--- pkgs/hack-nerdfonts/fonts.nix
+++ pkgs/hack-nerdfonts/fonts.nix
@@ -3,24 +3,24 @@
     name = "HackNERDFonts-Bold.ttf";
     url =
       "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf";
-    sha256 = "1mrdqdqjvh3qz7vsq53lb5c6awrybpnp8frk66644g1xklsnbwzz";
+    sha256 = "12yfjmlw1cr9c8rgblagv6g88sca1c9rfq1sdcyfl654lw8mrpzx";
   }
   {
     name = "HackNERDFonts-Italic.ttf";
     url =
       "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Italic/complete/Hack%20Italic%20Nerd%20Font%20Complete.ttf";
-    sha256 = "0pwzam9gs8b0aqvxmhhf945gldj2p18mdhmd232z98dwfbv40cmg";
+    sha256 = "079ncnsr9gwajbbra6zkwy255z1wzahimrarcj70p120z8hx22bb";
   }
   {
     name = "HackNERDFonts-Regular.ttf";
     url =
       "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf";
-    sha256 = "10fw2mf39cpzdvwdkhx096phajwf92mzzc80skpyy2nq5lapdcs1";
+    sha256 = "106cd74frrlkag7vqplm8gz40gj273z07ymcm6nmxhx11zwhmc7c";
   }
   {
     name = "BoldItalic.ttf";
     url =
       "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/BoldItalic/complete/Hack%20Bold%20Italic%20Nerd%20Font%20Complete.ttf";
-    sha256 = "00kw0avaxmqas56g58mzlxr4vkl8f723xpc58iphrirjjxh9w6gn";
+    sha256 = "0zpqksq0bzhvwp3cj97lzjjq7j2b6yrd2bqixzhdxglcab90cma4";
   }
 ]

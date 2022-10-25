88c19c2b73977a6d6c6d6977d9270478b329a303
nyarla <nyarla@kalaclista.com>
2022-10-25 10:13:10 +0900

    fix: add workaround patch for android-tools fail to build on master

--- pkgs/temporary.nix
+++ pkgs/temporary.nix
@@ -16,6 +16,16 @@ in {
     '';
   });
 
+  android-tools = super.android-tools.overrideAttrs (old: rec {
+    patches = old.patches ++ [
+      (super.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/NixOS/nixpkgs/f5db877954c6839a2de43b3e2a4f70f0836fad3b/pkgs/tools/misc/android-tools/android-tools-kernel-headers-6.0.diff";
+        sha256 = "1wlxlb6p9n6ppn0ikwnl023lsdgvlhshaykj91asqa5lip1rzqyz";
+      })
+    ];
+  });
+
   tt-rss = super.tt-rss.overrideAttrs (old: rec {
     pname = "tt-rss";
     version = "2022-10-08";

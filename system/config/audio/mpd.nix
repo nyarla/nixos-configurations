9e86dcc31a85fdab26376ecb23ee9f01531a7f6a
nyarla <nyarla@kalaclista.com>
2022-10-25 10:10:27 +0900

    chore: set device ids to kernel parameter for Scarlett Solo 2Gen

--- system/config/audio/mpd.nix
+++ system/config/audio/mpd.nix
@@ -1,1 +1,7 @@
-{ pkgs, lib, ... }: { boot.kernelParams = [ "snd_usb_audio.index=11" ]; }
+{ pkgs, lib, ... }: {
+  boot.kernelParams = [
+    "snd_usb_audio.index=11"
+    "snd_usb_audio.vid=0x1235"
+    "snd_usb_audio.pid=0x8205"
+  ];
+}

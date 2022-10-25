095adbf95dcc66cd77baf4086f82261995e44fec
nyarla <nyarla@kalaclista.com>
2022-10-25 11:36:02 +0900

    chore(pkgs): remove workaround for quodlibet and cnijfilter2

--- pkgs/temporary.nix
+++ pkgs/temporary.nix
@@ -1,15 +1,6 @@
 self: super:
 let require = path: super.callPackage (import path);
 in {
-  quodlibet = super.quodlibet.overrideAttrs (old: rec {
-    doInstallCheck = false;
-    nativeBuildInputs = old.nativeBuildInputs
-      ++ [ super.gobject-introspection ];
-  });
-
-  cnijfilter2 = super.cnijfilter2.overrideAttrs
-    (old: rec { NIX_CFLAGS_COMPILE = " -fcommon"; });
-
   whipper = super.whipper.overrideAttrs (old: rec {
     postPatch = ''
       sed -i 's|cd-paranoia|${super.cdparanoia}/bin/cdparanoia|g' whipper/program/cdparanoia.py

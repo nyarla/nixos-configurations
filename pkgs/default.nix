4d31c68e6832c5a799531d7c7851b60644e709af
nyarla <nyarla@kalaclista.com>
2022-10-25 10:15:53 +0900

    chore(tt-rss): move overrides to pkg/defaults.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -113,6 +113,36 @@ in {
   thunderbird-bin-unwrapped =
     super.thunderbird-bin-unwrapped.override { systemLocale = "ja_JP"; };
 
+  tt-rss = super.tt-rss.overrideAttrs (old: rec {
+    pname = "tt-rss";
+    version = "2022-10-08";
+    src = super.fetchgit {
+      url = "https://git.tt-rss.org/fox/tt-rss.git";
+      rev = "68dee4578230306da895d5090b7997d8dd23952e";
+      sha256 = "1pr1wx0bg79hivrmlw2gvjprlmvvrc00jai7bi7yy96s5vns8jwz";
+    };
+
+    installPhase = ''
+      runHook preInstall
+      mkdir $out
+      cp -ra * $out/
+      # see the code of Config::get_version(). you can check that the version in
+      # the footer of the preferences pages is not UNKNOWN
+      echo "22.10" > $out/version_static.txt
+      runHook postInstall
+    '';
+  });
+
+  tt-rss-theme-feedly = super.tt-rss-theme-feedly.overrideAttrs (old: rec {
+    version = "2022-10-08";
+    src = super.fetchFromGitHub {
+      owner = "levito";
+      repo = "tt-rss-feedly-theme";
+      rev = "d9657718eb4a8aeabffab7660ea336c72db811fb";
+      sha256 = "1isvkxsj1wn77xghrfxjgqgh9vrnw8cqs4gbg12fj4nlzk66nsmb";
+    };
+  });
+
   wlroots = super.wlroots.overrideAttrs (old: rec {
     version = "2022-08-28";
     src = super.fetchFromGitLab {

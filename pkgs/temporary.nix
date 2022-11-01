e31d7f973dbe6f360dfa76b0cea370ca2c1717d7
nyarla <nyarla@kalaclista.com>
2022-11-01 12:02:20 +0900

    chore: update nix flake

--- pkgs/temporary.nix
+++ pkgs/temporary.nix
@@ -6,14 +6,4 @@ in {
       sed -i 's|cd-paranoia|${super.cdparanoia}/bin/cdparanoia|g' whipper/program/cdparanoia.py
     '';
   });
-
-  android-tools = super.android-tools.overrideAttrs (old: rec {
-    patches = old.patches ++ [
-      (super.fetchpatch {
-        url =
-          "https://raw.githubusercontent.com/NixOS/nixpkgs/f5db877954c6839a2de43b3e2a4f70f0836fad3b/pkgs/tools/misc/android-tools/android-tools-kernel-headers-6.0.diff";
-        sha256 = "1wlxlb6p9n6ppn0ikwnl023lsdgvlhshaykj91asqa5lip1rzqyz";
-      })
-    ];
-  });
 }

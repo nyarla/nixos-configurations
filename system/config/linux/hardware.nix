1e73869740fb2dcb81dcf2dc2067a86c4ba89e18
nyarla <nyarla@kalaclista.com>
2022-11-07 19:24:18 +0900

    chore: optimize storage performance

--- system/config/linux/hardware.nix
+++ system/config/linux/hardware.nix
@@ -1,3 +1,9 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [ multipath-tools pciutils usbutils ];
+
+  services.udev.extraRules = ''
+    ACTION=="add|change", KERNEL=="nvme[0-9]n[0-9]", ATTR{queue/scheduler}="none"
+    ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
+    ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+  '';
 }

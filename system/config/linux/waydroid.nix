8de939aa89a86f5adf1ee807a106471bcdf465e3
nyarla <nyarla@kalaclista.com>
2022-11-15 16:13:36 +0900

    feat: add waydroid configuration

---
+++ system/config/linux/waydroid.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, ... }: {
+  virtualisation = {
+    waydroid.enable = true;
+    lxd.enable = true;
+  };
+
+  environment.etc."gbinder.d/waydroid.conf".source = lib.mkForce
+    (pkgs.writeText "waydroid.conf" ''
+      [Protocol]
+      /dev/binder = aidl3
+      /dev/vndbinder = aidl3
+      /dev/hwbinder = hidl
+
+      [ServiceManager]
+      /dev/binder = aidl3
+      /dev/vndbinder = aidl3
+      /dev/hwbinder = hidl
+
+      [General]
+      ApiLevel = 30
+    '');
+}

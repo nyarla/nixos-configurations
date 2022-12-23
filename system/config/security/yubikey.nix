b30ab60181bf436cdbba5c230913affc7035acae
nyarla <nyarla@kalaclista.com>
2022-12-23 10:49:32 +0900

    feat: add yubikey configuration

---
+++ system/config/security/yubikey.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    yubikey-manager
+    yubikey-manager-qt
+    yubikey-personalization
+    yubikey-personalization-gui
+  ];
+
+  services.udev.packages = [ pkgs.yubikey-personalization ];
+  services.pcscd.enable = true;
+
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
+}

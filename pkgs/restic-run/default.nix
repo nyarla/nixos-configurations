14c7e44af3237686831ea7e5f8b4279327584e3d
nyarla <nyarla@kalaclista.com>
2023-01-05 16:09:27 +0900

    chore: rewrite restic-run

--- pkgs/restic-run/default.nix
+++ pkgs/restic-run/default.nix
@@ -1,45 +1,15 @@
 { runCommand, writeShellScript, proot, restic, rclone, lib }:
 let
-  restic-exec = writeShellScript "restic-exec.sh" ''
-    set -euo pipefail
-
-    export PATH=${lib.makeBinPath [ restic rclone proot ]}:$PATH
-
-    DIR="$1"
-
-    if test "z$DIR" = "z"; then
-      echo "Usage: restic-run name [...command]" >&2
-      exit 1
-    fi
-
-    export HOME=/home/nyarla
-
-    export RESTIC_PASSWORD_FILE=$HOME/.config/rclone/restic
-    export RESTIC_REPOSITORY=rclone:Teracloud:Restic/$DIR
-
-    shift 1
-
-    exec "''${@}"
-  '';
-
   restic-run = writeShellScript "restic-run.sh" ''
     set -euo pipefail
 
     export PATH=${lib.makeBinPath [ restic rclone ]}:$PATH
 
-    DIR="$1"
-
-    if test "z$DIR" = "z"; then
-      echo "Usage: restic-run name [...command]" >&2
-      exit 1
-    fi
-
     export HOME=/home/nyarla
 
     export RESTIC_PASSWORD_FILE=$HOME/.config/rclone/restic
-    export RESTIC_REPOSITORY=rclone:Teracloud:Restic/$DIR
+    export RESTIC_REPOSITORY=rclone:Teracloud:Backup/NixOS
 
-    shift 1
     exec "''${@}"
   '';
 
@@ -58,17 +28,16 @@ let
 
     shift 1
 
-    ${restic-run} "$DIR" ${restic}/bin/restic cache --cleanup
-    ${restic-run} "$DIR" ${restic}/bin/restic unlock --remove-all
-    ${restic-run} "$DIR" ${restic}/bin/restic rebuild-index
-    ${restic-run} "$DIR" ${restic}/bin/restic prune
-    ${restic-run} "$DIR" ${restic}/bin/restic backup $RESTIC_BACKUP_ARGS "$DIR"
-    ${restic-run} "$DIR" ${restic}/bin/restic forget $RESTIC_FORGET_ARGS
+    ${restic-run} ${restic}/bin/restic cache --cleanup
+    ${restic-run} ${restic}/bin/restic unlock --remove-all
+    ${restic-run} ${restic}/bin/restic rebuild-index
+    ${restic-run} ${restic}/bin/restic prune
+    ${restic-run} ${restic}/bin/restic backup $RESTIC_BACKUP_ARGS "$DIR"
+    ${restic-run} ${restic}/bin/restic forget $RESTIC_FORGET_ARGS
   '';
 in runCommand "restic-run" { } ''
   mkdir -p $out/bin
   cp ${restic-backup} $out/bin/restic-backup
-  cp ${restic-exec} $out/bin/restic-exec
   cp ${restic-run} $out/bin/restic-run
   chmod +x $out/bin/*
 ''

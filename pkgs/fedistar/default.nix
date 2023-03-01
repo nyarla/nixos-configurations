a34d1e385bf3e62bea612b0a90f274ea7e3f25f5
nyarla <nyarla@kalaclista.com>
2023-03-01 14:49:33 +0900

    chore: update fedistar

--- pkgs/fedistar/default.nix
+++ pkgs/fedistar/default.nix
@@ -2,12 +2,12 @@
 , webkitgtk, wrapGAppsHook }:
 stdenv.mkDerivation rec {
   name = "fedistar";
-  version = "0.8.3";
+  version = "1.0.0-beta1";
 
   src = fetchurl {
     url =
-      "https://github.com/h3poteto/fedistar/releases/download/v${version}/fedistar_${version}_amd64.deb";
-    sha256 = "sha256-IS3aJiHLApc5A75XIKABAFCNp1H4hO4AIjDbnRX33Iw=";
+      "https://github.com/h3poteto/fedistar/releases/download/v1.0.0-beta.1/fedistar_1.0.0_amd64.deb";
+    sha256 = "1bfsj95d0bdvkz8ckdbzfng84x4lrf0d66mnfs9v3bx1kfgndvf2";
   };
 
   nativeBuildInputs = [ autoPatchelfHook dpkg ];

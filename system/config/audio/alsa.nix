1ba44712ac49bf179af8fee9fb930afdfb68f6a0
nyarla <nyarla@kalaclista.com>
2023-03-21 10:10:00 +0900

    chore: update audio/alsa.nix

--- system/config/audio/alsa.nix
+++ system/config/audio/alsa.nix
@@ -3,9 +3,12 @@
 
   boot.kernelModules = [ "snd_aloop" ];
   boot.kernelParams = [
-    "snd_aloop.index=10,20,21,22"
-    "snd_aloop.id=DroidCam,44100Hz,48000Hz,96000Hz"
-    "snd_aloop.enable=1,1,1,1"
+    "snd_usb_audio.index=11"
+    "snd_usb_audio.vid=0x1235"
+    "snd_usb_audio.pid=0x8205"
+    "snd_aloop.index=10,20,21,22,23"
+    "snd_aloop.id=DroidCam,CD,DVD,HiFi,Ultra"
+    "snd_aloop.enable=1,1,1,1,1"
   ];
 
   environment.systemPackages = with pkgs; [ apulse ];

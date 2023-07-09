a9c63db703a8112d861d23427a045946325e4e9b
nyarla <nyarla@kalaclista.com>
2023-07-09 15:19:33 +0900

    chore: improve pstate configration for amd

--- system/config/cpu/amd.nix
+++ system/config/cpu/amd.nix
@@ -1,1 +1,22 @@
-_: { hardware.cpu.amd.updateMicrocode = true; }
+{ lib, config, ... }:
+let
+  kver = config.boot.kernelPackages.kernel.version;
+  older = lib.versionOlder kver;
+  least = lib.versionAtLeast kver;
+in {
+  hardware.cpu.amd.updateMicrocode = true;
+
+  # referernces from https://github.com/NixOS/nixos-hardware/blob/master/common/cpu/amd/pstate.nix
+  boot = lib.mkMerge [
+    (lib.mkIf ((least "5.17") && (older "6.1")) {
+      kernelParams = [ "initcall_blacklist=acpi_cpufreq_init" ];
+      kernelModules = [ "amd-pstate" ];
+    })
+
+    (lib.mkIf ((least "6.1") && (older "6.3")) {
+      kernelParams = [ "amd_pstate=passive" ];
+    })
+
+    (lib.mkIf (least "6.3") { kernelParams = [ "amd_pstate=passive" ]; })
+  ];
+}

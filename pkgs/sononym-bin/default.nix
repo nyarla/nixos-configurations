1952431b1b70085536473ef7b2fbcef0c1a73d65
nyarla <nyarla@kalaclista.com>
2023-07-27 11:24:05 +0900

    chore: use autoPatchelfHook to sononym

--- pkgs/sononym-bin/default.nix
+++ pkgs/sononym-bin/default.nix
@@ -1,72 +1,96 @@
-{ stdenv, fetchurl, buildFHSUserEnv, writeShellScript }:
-let
-  pkg = stdenv.mkDerivation rec {
-    pname = "sononym";
-    version = "1.4.2";
-    src = fetchurl {
-      url = "https://www.sononym.net/download/sononym-1.4.2.tar.bz2";
-      sha256 = "08xwplvma4vvhkwcdwx2b1gy01mcgcdd7mgw2nz2cbdxq4rq9w08";
-    };
+{ stdenv, lib, fetchurl, autoPatchelfHook, makeWrapper, alsa-lib, at-spi2-atk
+, at-spi2-core, atk, cairo, cups, dbus, expat, fontconfig, freetype, gdk-pixbuf
+, glib, gtk3, libappindicator-gtk3, libcap, libdbusmenu, libdrm, libglvnd
+, libgpg-error, libjack2, libnotify, libsecret, libuuid, libxkbcommon, mesa
+, nspr, nss, pango, systemd, unzip, xorg, zlib }:
+stdenv.mkDerivation rec {
+  pname = "sononym";
+  version = "1.4.2";
 
-    phases = [ "unpackPhase" "installPhase" ];
-
-    installPhase = ''
-      mkdir -p $out/share/sononym 
-      cp -R . $out/share/sononym/
-    '';
+  src = fetchurl {
+    url = "https://www.sononym.net/download/sononym-${version}.tar.bz2";
+    sha256 = "08xwplvma4vvhkwcdwx2b1gy01mcgcdd7mgw2nz2cbdxq4rq9w08";
   };
-in buildFHSUserEnv rec {
-  name = "sononym";
-  targetPkgs = pkgs:
-    with pkgs; [
-      alsa-lib
-      at-spi2-atk
-      at-spi2-core
-      atk
-      cairo
-      cups
-      dbus
-      expat
-      fontconfig
-      freetype
-      gdk-pixbuf
-      glib
-      gtk3
-      libappindicator-gtk3
-      libcap
-      libdbusmenu
-      libdrm
-      libglvnd
-      libgpg-error
-      libjack2
-      libnotify
-      libsecret
-      libuuid
-      libxkbcommon
-      mesa
-      nspr
-      nss
-      pango
-      stdenv.cc.cc
-      systemd
-      unzip
-      xorg.libX11
-      xorg.libXScrnSaver
-      xorg.libXcomposite
-      xorg.libXcursor
-      xorg.libXdamage
-      xorg.libXext
-      xorg.libXfixes
-      xorg.libXi
-      xorg.libXrandr
-      xorg.libXrender
-      xorg.libXtst
-      xorg.libxcb
-      xorg.libxkbfile
-      xorg.libxshmfence
-      zlib
-    ];
-  runScript = writeShellScript "sononym-bin.sh" ''
-    exec -a "''${0}" ${pkg}/share/sononym/sononym "$@" 
+
+  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
+
+  buildInputs = [
+    alsa-lib
+    at-spi2-atk
+    at-spi2-core
+    atk
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig.out
+    freetype
+    gdk-pixbuf
+    glib.out
+    gtk3
+    libappindicator-gtk3
+    libcap
+    libdbusmenu
+    libdrm
+    libglvnd
+    libgpg-error
+    libjack2
+    libnotify
+    libsecret
+    libuuid.out
+    libxkbcommon
+    mesa
+    nspr
+    nss
+    pango.out
+    stdenv.cc.cc
+    stdenv.cc.libc
+    stdenv.cc.cc.lib
+    systemd
+    unzip
+    zlib
+  ] ++ (with xorg; [
+    libX11
+    libXScrnSaver
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXtst
+    libxcb
+    libxkbfile
+    libxshmfence
+  ]);
+
+  libPath = lib.makeLibraryPath buildInputs
+    + (":" + lib.makeSearchPathOutput "lib" "lib64" buildInputs);
+
+  buildPhase = ''
+    runHook preBuild
+
+    mkdir -p $out/opt/sononym
+    cp -r . $out/opt/sononym
+
+    runHook postBuild
+  '';
+
+  dontStirp = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    ln -sf $out/opt/sononym/sononym $out/bin/sononym
+
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    wrapProgram $out/bin/sononym \
+      --prefix LD_LIBRARY_PATH : "${libPath}:$out/opt/sononym"
   '';
 }

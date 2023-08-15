da3dd8203e09343ca76f16b280971ebb1a5c2e2a
nyarla <nyarla@kalaclista.com>
2023-08-15 10:44:22 +0900

    feat: add Vial keybord firmware manager

--- system/config/keyboard/zinc.nix
+++ system/config/keyboard/zinc.nix
@@ -1,8 +1,14 @@
-_: {
+{ pkgs, ... }: {
   boot.kernelModules = [ "uinput" ];
+
+  hardware.keyboard.qmk.enable = true;
 
   services.udev.extraRules = ''
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="04d8", ATTRS{idProduct}=="ea3b", ACTION=="add", GROUP="users", MODE="660"
     KERNEL=="uinput", GROUP="input", MODE="660"
   '';
+
+  services.udev.packages = with pkgs; [ vial ];
+
+  environment.systemPackages = with pkgs; [ qmk vial ];
 }

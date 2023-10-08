2f1884d11a58da30723a199817c55e38288a049e
nyarla <nyarla@kalaclista.com>
2023-10-08 11:26:07 +0900

    fix: temporary fix for fail to build weston

--- pkgs/temporary.nix
+++ pkgs/temporary.nix
@@ -1,6 +1,8 @@
 self: super:
 let require = path: super.callPackage (import path);
 in {
+  weston = super.weston.override { vncSupport = false; };
+
   whipper = super.whipper.overrideAttrs (old: rec {
     postPatch = ''
       sed -i 's|cd-paranoia|${super.cdparanoia}/bin/cdparanoia|g' whipper/program/cdparanoia.py

3c1d272898113930ef64cf0ed33053b9f8200322
nyarla <nyarla@kalaclista.com>
2023-11-07 12:38:27 +0900

    chore: update desktop theme settings

--- system/config/desktop/theme.nix
+++ system/config/desktop/theme.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, ... }: {
   xdg.icons.enable = true;
 
   environment.sessionVariables = {
@@ -9,8 +9,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-    adwaita-qt
-    adwaita-qt6
     capitaine-cursors
     fluent-gtk-theme
     fluent-icon-theme
@@ -20,20 +18,10 @@
     gtk_engines
     hicolor-icon-theme
     kaunas
-    libsForQt5.qtstyleplugin-kvantum
-    (libsForQt5.qtstyleplugin-kvantum.overrideAttrs (_: rec {
-      pname = "qtstyleplugin-kvantum-qt6";
-      nativeBuildInputs = [ cmake qt6.qttools ];
-      buildInputs = [ qt6.qtsvg ];
-      cmakeFlags = [ "-DENABLE_QT5=off" ];
-
-      postPatch = ''
-        sed -i "s!\''${_Qt6_PLUGIN_INSTALL_DIR}!$out/${qt6.qtbase.qtPluginPrefix}!" style/CMakeLists.txt
-      '';
-
-      dontWrapQtApps = true;
-    }))
-    qgnomeplatform
-    qgnomeplatform-qt6
   ];
+
+  qt = {
+    enable = true;
+    style = "kvantum";
+  };
 }

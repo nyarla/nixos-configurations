cccba79187900622b3159f42754afa26824fd7ed
nyarla <nyarla@kalaclista.com>
2023-11-10 11:16:26 +0900

    chore: use wine-staging 8.19

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -144,5 +144,13 @@ in {
     patches = [ ../patches/wlroots-workaround.patch ];
   });
 
-  wineUsingFull = super.wineWowPackages.stagingFull;
+  wineUsingFull = super.lib.overrideDerivation super.wineWowPackages.stagingFull
+    (old: rec {
+      buildInputs = old.buildInputs ++ (with super; [ libgcrypt.dev libva.dev ])
+        ++ (with super.pkgsi686Linux; [ libgcrypt.dev libva.dev ]);
+
+      configureFlags = old.configureFlags
+        ++ [ "--with-va" "--with-gcrypt" "--disable-test" ];
+    });
+  #wineUsingFull = super.wineWowPackages.stagingFull;
 }

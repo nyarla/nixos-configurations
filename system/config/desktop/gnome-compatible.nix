3892a0de243e85fd8eeabb5c7cc8cc7913b9c320
nyarla <nyarla@kalaclista.com>
2023-11-25 12:24:39 +0900

    chore(system/gnome-compatible): cleanup code

--- system/config/desktop/gnome-compatible.nix
+++ system/config/desktop/gnome-compatible.nix
@@ -1,21 +1,25 @@
 { config, pkgs, ... }: {
   environment.systemPackages =
     (with pkgs.gnome; [ zenity dconf-editor gnome-keyring ])
-    ++ (with pkgs; [ gtk3 dconf gsound gcr ]);
+    ++ (with pkgs; [ gsound gcr ]);
 
-  services.dbus.enable = true;
+  services = {
+    accounts-daemon.enable = true;
+    dbus.enable = true;
+    system-config-printer.enable = true;
+
+    gnome = {
+      gnome-settings-daemon.enable = true;
+      glib-networking.enable = true;
+    };
+  };
 
   programs.dconf.enable = true;
   programs.seahorse.enable = true;
 
-  services.accounts-daemon.enable = true;
-  services.gnome.gnome-settings-daemon.enable = true;
-  services.gnome.glib-networking.enable = true;
-  services.system-config-printer.enable = true;
   services.upower.enable = config.powerManagement.enable;
 
   security.pam.services.keyring.enableGnomeKeyring = true;
-
   security.wrappers.gnome-keyring-daemon = {
     owner = "root";
     group = "root";

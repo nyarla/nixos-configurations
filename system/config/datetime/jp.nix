ecee1fa3b25f10f29aa4e191083b6ad8fc4e0ede
nyarla <nyarla@kalaclista.com>
2023-11-25 11:48:39 +0900

    chore(datetime): improve time sync by ntp

--- system/config/datetime/jp.nix
+++ system/config/datetime/jp.nix
@@ -3,7 +3,9 @@ _: {
   time.timeZone = "Asia/Tokyo";
 
   # ntp sync
-  services.timesyncd.enable = true;
-  networking.timeServers =
-    [ "ntp1.jst.mfeed.ad.jp" "ntp2.jst.mfeed.ad.jp" "ntp3.jst.mfeed.ad.jp" ];
+  services.timesyncd = {
+    enable = true;
+    servers =
+      [ "ntp1.jst.mfeed.ad.jp" "ntp2.jst.mfeed.ad.jp" "ntp3.jst.mfeed.ad.jp" ];
+  };
 }

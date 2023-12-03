a767a49cfe37edea0c336286dd86c438b29341d1
nyarla <nyarla@kalaclista.com>
2023-12-03 13:10:23 +0900

    refactor(xdg): improve xdg-desktop-portal settings

--- system/config/desktop/xdg.nix
+++ system/config/desktop/xdg.nix
@@ -1,10 +1,20 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    xdg-utils
-    xdg-desktop-portal
-    xdg-desktop-portal-gtk
-  ];
+  environment.systemPackages = with pkgs; [ xdg-utils ];
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    configPackages = with pkgs; [ xdg-desktop-portal-gtk ];
 
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    wlr = {
+      enable = true;
+      settings = {
+        screencast = {
+          output_name = "HDMI-A-1";
+          max_fps = 30;
+          chooser_type = "simple";
+          chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+        };
+      };
+    };
+  };
 }

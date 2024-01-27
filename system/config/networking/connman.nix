2ce67e46fcfaa1a2dbc66e09d3d3ee0fc817328d
nyarla <nyarla@kalaclista.com>
2024-01-27 14:13:15 +0900

    chore(connman): add vboxnet interface to blocklist

--- system/config/networking/connman.nix
+++ system/config/networking/connman.nix
@@ -3,7 +3,7 @@
   services.connman.package = pkgs.connmanFull;
   services.connman.wifi.backend = "iwd";
   services.connman.networkInterfaceBlacklist =
-    [ "tailscale" "docker" "veth" "virbr" ];
+    [ "tailscale" "docker" "veth" "virbr" "vboxnet" ];
 
   environment.systemPackages = with pkgs; [ connman-gtk ];
   networking.resolvconf.enable = false;

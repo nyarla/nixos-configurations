ce1de86093eeaec7a5071b457f6de3a36baa0115
nyarla <nyarla@kalaclista.com>
2024-03-03 10:08:21 +0900

    fix(tailscale): use nixpkgs's autoconnect to tailscale network

--- system/config/networking/tailscale.nix
+++ system/config/networking/tailscale.nix
@@ -1,21 +1,6 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ tailscale ];
-  services.tailscale.enable = true;
-
-  systemd.services.tailscale-autoconnect = {
-    description = "Auto-connect to tailscale";
-    after = [ "network-pre.target" "tailscaled.service" ];
-    wants = [ "network-pre.target" "tailscaled.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = { Type = "oneshot"; };
-    script = with pkgs; ''
-      TRIES=0
-      until ${tailscale}/bin/tailscale up -ssh ; do
-        sleep 0.1
-        let TRIES+=1
-
-        test $TRIES = 20 && break
-      done
-    '';
+_: {
+  services.tailscale = {
+    enable = true;
+    extraUpFlags = [ "--ssh" ];
   };
 }

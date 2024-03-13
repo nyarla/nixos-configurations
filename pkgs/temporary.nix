988e68eddfa12769c545e94fb5146823057b8b4e
nyarla <nyarla@kalaclista.com>
2024-03-13 19:20:53 +0900

    chore(flake): update nix flake at 2024-03-13

--- pkgs/temporary.nix
+++ pkgs/temporary.nix
@@ -1,22 +1,8 @@
-self: super:
+_: super:
 let require = path: super.callPackage (import path);
 in {
-  cmake-3_28_3 = super.cmake.overrideAttrs (old: rec {
-    inherit (old) pname;
-    version = "3.28.3";
-    src = super.fetchurl {
-      url = "https://cmake.org/files/v${
-          super.lib.versions.majorMinor version
-        }/cmake-${version}.tar.gz";
-      hash = "sha256-crdXDlyFk95qxKtDO3PqsYxfsyiIBGDIbOMmCBQa1cE=";
-    };
-  });
-
-  musescore = super.musescore.override { cmake = self.cmake-3_28_3; };
-
   weston = super.weston.override { vncSupport = false; };
-
-  whipper = super.whipper.overrideAttrs (old: rec {
+  whipper = super.whipper.overrideAttrs (_: rec {
     postPatch = ''
       sed -i 's|cd-paranoia|${super.cdparanoia}/bin/cdparanoia|g' whipper/program/cdparanoia.py
     '';

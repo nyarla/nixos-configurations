7073fd0aa8d8aded80848fe50a590045dfa0503b
nyarla <nyarla@kalaclista.com>
2024-03-18 19:16:56 +0900

    feat(desktop-panel): use waybar as desktop-panel on wayland session

--- dotfiles/config/desktop/desktop-panel.nix
+++ dotfiles/config/desktop/desktop-panel.nix
@@ -12,13 +12,14 @@
       Environment = [
         "HOME=${config.home.homeDirectory}"
         "LANG=ja_JP.UTF-8"
+        "LC_TIME=ja_JP.UTF-8"
         "LC_ALL=ja_JP.UTF-8"
         "XDG_DATA_DIRS=${config.home.profileDirectory}/share"
       ];
       ExecStart = toString (pkgs.writeShellScript "panel" ''
         if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
           export DISPLAY=:0
-          ${pkgs.sfwbar}/bin/sfwbar &
+          ${pkgs.waybar}/bin/waybar &
           export waidPID=$!
         else
           ${pkgs.lxqt.lxqt-panel}/bin/lxqt-panel 2>/dev/null &
@@ -31,150 +32,55 @@
     };
   };
 
-  xdg.configFile."sfwbar/sfwbar.config".text = ''
-    function("SfwbarInit") {
-      SetLayer "overlay"
-      SetMonitor "HDMI-1"
-    }
-
-    function("ToggleMinimize") {
-      [!Minimized] Minimize
-      [Minimized] UnMinimize
-    }
-
-    function("ToggleMaximize") {
-      [!Maximized] Maximize
-      [Maximized] UnMaximize
-    }
-
-    function("ToggleRaise") {
-      [Minimized] UnMinimize
-      [!Minimized | !Focused] Focus
-      [!Minimized | Focused] Minimize
-    }
-
-    menu("winops") {
-      item("focus", Focus );
-      item("close", Close );
-      item("(un)minimize", Function "ToggleMinimize" );
-      item("(un)maximize", Function "ToggleMaximize" );
-    }
-
-    layout {
-      button "launch" {
-        action = Exec "ydotool key 56:1 59:1 59:0 56:0"
-        value = "terminal"
-        css = "* { min-width: 16px; min-height: 16px; margin: 0 4px; padding: 0 4px; border-radius: 3px; border-color: transparent; }"
-      }
-
-      taskbar {
-        icons = true
-        labels = false
-        rows = 1
-        action[1] = Function "ToggleRaise"
-        action[3] = Menu "winops"
-      }
-
-      label {
-        css = "* { -GtkWidget-hexpand: true; }"
-        value = ""
-      }
-      
-      tray {}
-
-      label {
-        style = "datetime"
-        interval = 1000
-        value = Time("%Y-%m-%d %H:%M:%S（%a）")
-        action[1] = Exec "env GDK_BACKEND=x11 galendae -c /home/nyarla/.config/galendae/config"
-      }
-    }
-
-    #CSS
-
-    #sfwbar {
-      background-color: #F5F6F7;
-      color: #000000;
-      outline-width: 0px;
-    }
-
-    #sfwbar box,
-    #sfwbar widget,
-    #sfwbar button {
-        outline-width: 0px;
-        padding-bottom: 0px;
-        padding-top: 0px;
-    }
-
-    #sfwbar grid{
-        padding-left: 4px;
-        min-width:20px;
-    }
-
-    #sfwbar button {
-      min-height: 16px ;
-      padding: 3px;
-    }
-
-    #sfwbar button image {
-      min-width: 16px;
-    }
-
-    window {
-      -GtkWidget-direction: top;
-    }
-
-    button#taskbar_normal image,
-    button#taskbar_normal:hover image,
-    button#taskbar_active image,
-    button#tray_normal image,
-    button#tray_nornal:hover image,
-    button#tray_active image {
+  xdg.configFile."waybar/style.css".text = ''
+    * image {
       -GtkWidget-vexpand: true;
     }
 
-    button#taskbar_normal image,
-    button#taskbar_normal:hover image,
-    button#taskbar_active image {
-      padding: 0px ;
-    }
-
-    button#taskbar_normal,
-    button#taskbar_normal:hover,
-    button#taskbar_active {
-      margin: 0px;
-
-      background-image: none;
-      
-      border-image: none;
-      border-radius: 2px ;
-      border-color: transparent;
-    }
-
-    button#tray_normal,
-    button#tray_nornal:hover,
-    button#tray_active {
-      border: none;
-    }
-
-    button#launch:hover,
-    button#taskbar_normal:hover {
-      background-color: #CCCCCC;
-    }
-
-    button#taskbar_active {
-      border-color: #999999;
-    }
-
-    label#datetime {
-      font-size: 12px;
-      font-weight: bold;
-      min-height: 24px;
-      min-width: 160px;
-      padding-bottom; 4px;
-      padding-left: 4px;
+    #tray {
+      padding-right: 4px;
     }
   '';
+
+  xdg.configFile."waybar/config".text = builtins.toJSON {
+    position = "top";
+    height = 28;
+    modules-left = [ "custom/app" "custom/creative" "wlr/taskbar" ];
+    modules-right = [ "tray" "clock" ];
+
+    "custom/app" = {
+      format = "  ";
+      on-click-release = "ydotool key 56:1 59:1 59:0 56:0";
+    };
+
+    "custom/creative" = {
+      format = "  ";
+      on-click-release = "ydotool key 56:1 60:1 60:0 56:0";
+    };
+
+    "wlr/taskbar" = {
+      format = "{icon}";
+      icon-size = 16;
+      icon-theme = "Fluent";
+      tooltip-format = "{title}";
+      on-click = "minimize-raise";
+      on-click-middle = "close";
+    };
+
+    "tray" = {
+      icon-size = 16;
+      spacing = 6;
+    };
+
+    "clock" = {
+      interval = 1;
+      format = "<b>{:%F %T（%a）}</b>";
+      timezone = "Asia/Tokyo";
+      locale = "ja_JP.UTF-8";
+      on-click-release =
+        "env GDK_BACKEND=x11 galendae -c /home/nyarla/.config/galendae/config";
+    };
+  };
 
   xdg.configFile."galendae/config".text = ''
     stick=0

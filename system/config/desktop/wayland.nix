19f2a488a75146d42b0c5efc515e47b0882e1e52
nyarla <nyarla@kalaclista.com>
2024-03-19 11:57:13 +0900

    chore(dbus): add waybar and xembed-sni-proxy to dbus related packages

--- system/config/desktop/wayland.nix
+++ system/config/desktop/wayland.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }: {
   imports = [ ./gnome-compatible.nix ];
   security.pam.services.swaylock = { };
+  services.dbus.packages = with pkgs; [ xembed-sni-proxy waybar ];
+  environment.systemPackages = with pkgs; [ xembed-sni-proxy waybar ];
 }

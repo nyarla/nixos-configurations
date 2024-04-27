059c47a9d6bc1171a63e56d6d5a531953087f8bf
nyarla <nyarla@kalaclista.com>
2024-04-27 13:37:07 +0900

    chore(calibre): remove workaround

--- pkgs/temporary.nix
+++ pkgs/temporary.nix
@@ -3,18 +3,6 @@ let
   require = path: super.callPackage (import path);
 in
 {
-  calibre =
-    let
-      python3 = super.python3.override {
-        packageOverrides = final: prev: {
-          mechanize = prev.mechanize.overrideAttrs (_: {
-            dontUsePytestCheck = true;
-          });
-        };
-      };
-    in
-    super.calibre.override { python3Packages = python3.pkgs; };
-
   weston = super.weston.override { vncSupport = false; };
   whipper = super.whipper.overrideAttrs (_: rec {
     postPatch = ''

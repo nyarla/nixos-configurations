5a585bb5b9c0428839e8b784ec9d1482de8fd84f
nyarla <nyarla@kalaclista.com>
2024-04-29 12:19:03 +0900

    chore(wine): use official wine-staging

--- flake.nix
+++ flake.nix
@@ -43,8 +43,7 @@
             hostname = "nixos";
             profile = import ./system/profile/NyZen9.nix;
             system = "x86_64-linux";
-
-            patches = [ ./patches/wine-staging.patch ];
+            patches = [ ];
             overlays = [
               wayland.overlay
               (import ./pkgs/default.nix)

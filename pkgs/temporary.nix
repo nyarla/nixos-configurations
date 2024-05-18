fa0e6ea3c62bb548856deca078143fcee6987c56
nyarla <nyarla@kalaclista.com>
2024-05-18 18:55:05 +0900

    chore(flake): update nix flake at 2024-05-18

--- pkgs/temporary.nix
+++ pkgs/temporary.nix
@@ -9,17 +9,4 @@ in
       sed -i 's|cd-paranoia|${super.cdparanoia}/bin/cdparanoia|g' whipper/program/cdparanoia.py
     '';
   });
-
-  pam_u2f = super.pam_u2f.override {
-    libfido2 = super.libfido2.override {
-      pcsclite = super.pcsclite.overrideAttrs (old: {
-        postPatch =
-          old.postPatch
-          + ''
-            substituteInPlace src/libredirect.c src/spy/libpcscspy.c \
-              --replace-fail "libpcsclite_real.so.1" "$lib/lib/libpcsclite_real.so.1"
-          '';
-      });
-    };
-  };
 }

a6645053c2c633c703e5b804f6f8172efdcbc97c
nyarla <nyarla@kalaclista.com>
2024-05-25 11:36:20 +0900

    chore(labwc): use Xwayland without eglstream

--- dotfiles/app/labwc/default.nix
+++ dotfiles/app/labwc/default.nix
@@ -73,74 +73,67 @@
     '';
   };
 
-  home.file.".local/bin/sw".source =
-    let
-      Xwayland = pkgs.writeShellScript "Xwayland" ''
-        exec -a Xwayland ${pkgs.xwayland}/bin/Xwayland "''${@}" -eglstream
-      '';
-    in
-    toString (
-      pkgs.writeShellScript "startlabwc" ''
-        for rc in $(ls /etc/profile.d); do
-          . /etc/profile.d/$rc
-        done
+  home.file.".local/bin/sw".source = toString (
+    pkgs.writeShellScript "startlabwc" ''
+      for rc in $(ls /etc/profile.d); do
+        . /etc/profile.d/$rc
+      done
 
-        for rc in $(ls $HOME/.config/profile.d); do
-          . $HOME/.config/profile.d/$rc
-        done
+      for rc in $(ls $HOME/.config/profile.d); do
+        . $HOME/.config/profile.d/$rc
+      done
 
-        export DESKTOP_SESSION=wlroots
-        export LIBSEAT_BACKEND=logind
+      export DESKTOP_SESSION=wlroots
+      export LIBSEAT_BACKEND=logind
 
-        export XDG_CURRENT_DESKTOP=wlroots
-        export XDG_SESSION_CLASS=user
-        export XDG_SESSION_DESKTOP=wlroots
-        export XDG_SESSION_TYPE=wayland
+      export XDG_CURRENT_DESKTOP=wlroots
+      export XDG_SESSION_CLASS=user
+      export XDG_SESSION_DESKTOP=wlroots
+      export XDG_SESSION_TYPE=wayland
 
-        export GBM_BACKEND=nvidia-drm
-        export GBM_BACKENDS_PATH=/etc/gbm
-        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export GBM_BACKEND=nvidia-drm
+      export GBM_BACKENDS_PATH=/etc/gbm
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
 
-        export LIBVA_DRIVER_NAME=nvidia
-        export VK_ICD_FILENAMES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/nvidia_icd.i686.json
-        export __EGL_VENDOR_LIBRARY_DIRS=/run/opengl-driver/share/glvnd/egl_vendor.d
-        export __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json
-        export __GL_GSYNC_ALLOWED=1
-        export __GL_VRR_ALLOWED=0
+      export LIBVA_DRIVER_NAME=nvidia
+      export VK_ICD_FILENAMES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/nvidia_icd.i686.json
+      export __EGL_VENDOR_LIBRARY_DIRS=/run/opengl-driver/share/glvnd/egl_vendor.d
+      export __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json
+      export __GL_GSYNC_ALLOWED=1
+      export __GL_VRR_ALLOWED=0
 
-        export WLR_NO_HARDWARE_CURSORS=1
-        export WLR_RENDERER_ALLOW_SOFTWARE=1
-        export WLR_RENDERER=gles2
-        export WLR_DRM_NO_ATOMIC=1
-        export WLR_XWAYLAND=${Xwayland}
-        export XWAYLAND_NO_GLAMOR=1
+      export WLR_NO_HARDWARE_CURSORS=1
+      export WLR_RENDERER_ALLOW_SOFTWARE=1
+      export WLR_RENDERER=gles2
+      export WLR_DRM_NO_ATOMIC=1
+      export XWAYLAND_NO_GLAMOR=1
 
+      if systemctl --user -q is-active desktop-session.target ; then
+        echo "Desktop session already exists." >&2
+        exit 1
+      fi
+
+      if hash dbus-update-activation-environment 2>/dev/null; then
+        dbus-update-activation-environment --systemd --all
+      fi
+
+      systemctl --user reset-failed
+
+      cleanup() {
+        pkill shoreman
         if systemctl --user -q is-active desktop-session.target ; then
-          echo "Desktop session already exists." >&2
-          exit 1
+          systemctl --user stop desktop-session.target
         fi
+      }
+      trap cleanup INT TERM
 
-        if hash dbus-update-activation-environment 2>/dev/null; then
-          dbus-update-activation-environment --systemd --all
-        fi
+      eval "$(dbus-launch --sh-syntax --exit-with-session)"
+      ${pkgs.labwc}/bin/labwc -V &
+      waidPID=$!
 
-        systemctl --user reset-failed
+      test -n $waidPID && wait $waidPID
 
-        cleanup() {
-          pkill shoreman
-          if systemctl --user -q is-active desktop-session.target ; then
-            systemctl --user stop desktop-session.target
-          fi
-        }
-        trap cleanup INT TERM
-
-        eval "$(dbus-launch --sh-syntax --exit-with-session)"
-        ${pkgs.labwc}/bin/labwc -V &
-        waidPID=$!
-
-        test -n $waidPID && wait $waidPID
-
-        cleanup
-      ''
-    );
+      cleanup
+    ''
+  );
 }

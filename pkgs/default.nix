fe8ba7e4c73d1d7b5dcd17fd4129d80fefca4b21
nyarla <nyarla@kalaclista.com>
2024-06-05 22:02:52 +0900

    refactor(pkgs): remove unused packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,9 +4,7 @@ let
 in
 {
   # additional packages
-  arc-openbox = require ./arc-openbox { };
   audiogridder = require ./audiogridder { };
-  clipboard-sync = require ./clipboard-sync { };
   cskk = require ./cskk { };
   cuda-shell = require ./cuda-shell {
     cudaPackages = super.cudaPackages_11_8;
@@ -14,39 +12,25 @@ in
   };
   currennt = require ./currennt { inherit (super.cudaPackages) cudatoolkit; };
   deadbeef-fb = require ./deadbeef-fb { };
-  elk-native = require ./elk-native { };
   fcitx5-fbterm = require ./fcitx5-fbterm { };
   fcitx5-cskk = super.libsForQt5.callPackage (import ./fcitx5-cskk) { };
   fcitx5-cskk-qt = self.fcitx5-cskk.override { enableQt = true; };
-  fedistar = require ./fedistar { };
-  flatery-icon-theme = require ./flatery-icon-theme { };
   galendae = require ./galendae { };
-  git-credential-keepassxc = require ./git-credential-keepassxc { };
   gyazo-diy = require ./gyazo-diy { };
   ildaeil = require ./ildaeil {
     inherit (self) carla;
     wine = self.wineUsingFull;
   };
-  jackass-bin = require ./jackass-bin { wine = self.wineUsingFull; };
-  juce-framework = require ./juce-framework { };
   kaunas = require ./kaunas { };
-  locale-eaw = require ./locale-eaw { };
-  mlterm-wrapped = require ./mlterm-wrapped { inherit (self) mlterm; };
   nvim-run = require ./nvim-run { };
   openjtalk = require ./openjtalk { };
   noto-fonts-jp = require ./noto-fonts-jp { };
-  novelai-manager = require ./novelai-manager { };
   perl-shell = require ./perl-shell { };
   restic-run = require ./restic-run { };
   shoreman = require ./shoreman { };
   skk-dicts-xl = require ./skk-dicts-xl { };
   sononym-bin = require ./sononym-bin { };
-  sysmontask = require ./sysmontask { };
-  tabby = require ./tabby { nvidia_x11 = null; };
-  terminfo-mlterm-256color = require ./terminfo-mlterm-256color { };
-  turbopilot = require ./turbopilot { nvidia_x11 = null; };
   vgpu_unlock-rs = require ./vgpu_unlock-rs { };
-  wcwidth-cjk = require ./wcwidth-cjk { };
   wine-run = require ./wine-run { };
   wine-vst-wrapper = require ./wine-vst-wrapper { };
   wineasio = require ./wineasio { wine = self.wineUsingFull; };
@@ -81,16 +65,6 @@ in
     };
   });
 
-  picom-next = super.picom-next.overrideAttrs (old: rec {
-    version = "2024-03-13";
-    src = super.fetchFromGitHub rec {
-      inherit (old.src) owner repo;
-      rev = "f7e538fe887ca96bbcf6549376ccf502f62a54c6";
-      hash = "sha256-eu7Zz/pWNWNdH4FUzdtTRsD/yD2vhWBfzewY6LE3UYo=";
-    };
-    buildInputs = old.buildInputs ++ [ super.xorg.xcbutil ];
-  });
-
   platinum-searcher = super.platinum-searcher.overrideAttrs (old: rec {
     src = super.fetchFromGitHub {
       inherit (old.src) owner repo;
@@ -108,37 +82,10 @@ in
     configureFlags = old.configureFlags ++ [ "--with-openjtalk" ];
   });
 
-  sfwbar = super.sfwbar.overrideAttrs (old: rec {
-    nativeBuildInputs = old.nativeBuildInputs ++ [ super.wrapGAppsHook ];
-  });
-
   thunderbird-bin-unwrapped = super.thunderbird-bin-unwrapped.override { systemLocale = "ja_JP"; };
 
   tmux = super.tmux.overrideAttrs (old: rec {
     patches = old.patches ++ [ ../patches/tmux-3.4-fix.diff ];
-  });
-
-  wlroots_0_17 = super.wlroots.overrideAttrs (old: rec {
-    src = super.fetchFromGitLab {
-      domain = "gitlab.freedesktop.org";
-      owner = "wlroots";
-      repo = "wlroots";
-      rev = "bc73e507abd1e8319353a8601f1f8ebfe17307ee";
-      hash = "sha256-jth6BKci3sVDC86o+gSHKyDWnibVcNmipm7nn0S6LTg=";
-    };
-
-    buildInputs =
-      old.buildInputs
-      ++ (with super; [
-        glslang
-        libdrm.dev
-        mesa.dev
-      ]);
-
-    # postPatch = ''
-    #   sed -i 's/glFlush/glFinish/' render/gles2/renderer.c
-    #   sed -i 's/glFlush/glFinish/' render/gles2/pass.c
-    # '';
   });
 
   # wineUsingFull = super.lib.overrideDerivation super.wineWowPackages.stagingFull (old: rec {

da451c3cf7d01cd2a5f5d50f3fc8fbeabe4a59c4
nyarla <nyarla@kalaclista.com>
2024-06-05 17:37:36 +0900

    fix(files): deduplicate thunder plugin package

--- system/config/desktop/files.nix
+++ system/config/desktop/files.nix
@@ -20,10 +20,7 @@ in
 
   programs.thunar = {
     enable = true;
-    plugins = with pkgs.xfce; [
-      thunar-archive-plugin
-      thunar-archive-plugin
-    ];
+    plugins = with pkgs.xfce; [ thunar-archive-plugin ];
   };
 
   programs.xfconf.enable = true;

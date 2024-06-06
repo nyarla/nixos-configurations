19e68e4c317ecc35c2215323bc2496829a73c403
nyarla <nyarla@kalaclista.com>
2024-06-06 19:15:50 +0900

    chore(mlterm): use new colorscheme

--- dotfiles/app/mlterm/color.nix
+++ dotfiles/app/mlterm/color.nix
@@ -1,19 +1,19 @@
 {
-  black = "#000000";
-  blue = "#00CCFF";
-  cyan = "#00CCCC";
-  green = "#CCFF00";
-  magenta = "#CC99CC";
-  red = "#FF6633";
-  white = "#F9F9F9";
-  yellow = "#FFCC33";
+  black = "#111111";
+  red = "#ff5661";
+  green = "#b0e300";
+  yellow = "#ffbf00";
+  blue = "#5fc7ff";
+  magenta = "#ff66ff";
+  cyan = "#00fff9";
+  white = "#f1f1f1";
 
-  hl_black = "#666666";
-  hl_blue = "#00CCFF";
-  hl_cyan = "#00CCCC";
-  hl_green = "#CCFF00";
-  hl_magenta = "#CC99CC";
-  hl_red = "#FF6633";
-  hl_white = "#FFFFFF";
-  hl_yellow = "#FFCC33";
+  hl_black = "#111111";
+  hl_red = "#ff5661";
+  hl_green = "#b0e300";
+  hl_yellow = "#ffbf00";
+  hl_blue = "#5fc7ff";
+  hl_magenta = "#ff66ff";
+  hl_cyan = "#00fff9";
+  hl_white = "#f1f1f1";
 }

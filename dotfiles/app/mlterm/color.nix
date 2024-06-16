d9ef23c26a2840521a57bfa24f85019073e9023e
nyarla <nyarla@kalaclista.com>
2024-06-16 16:56:45 +0900

    chore(mlterm): improve colorscheme

--- dotfiles/app/mlterm/color.nix
+++ dotfiles/app/mlterm/color.nix
@@ -1,19 +1,19 @@
 {
   black = "#111111";
-  red = "#ff5661";
+  red = "#ff4a6e";
   green = "#b0e300";
   yellow = "#ffbf00";
   blue = "#5fc7ff";
-  magenta = "#ff66ff";
+  magenta = "#efa6ff";
   cyan = "#00fff9";
   white = "#f1f1f1";
 
   hl_black = "#111111";
-  hl_red = "#ff5661";
+  hl_red = "#ff4a6e";
   hl_green = "#b0e300";
   hl_yellow = "#ffbf00";
   hl_blue = "#5fc7ff";
-  hl_magenta = "#ff66ff";
+  hl_magenta = "#efa6ff";
   hl_cyan = "#00fff9";
   hl_white = "#f1f1f1";
 }

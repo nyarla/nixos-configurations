6b4fe23a3e99875aa53efcc391ffa69e00adcef9
nyarla <nyarla@kalaclista.com>
2024-06-20 13:45:48 +0900

    fix(docker): use cloudflare dns as default container dns

--- system/config/linux/docker.nix
+++ system/config/linux/docker.nix
@@ -3,6 +3,12 @@
   virtualisation.docker = {
     enable = true;
     storageDriver = "overlay2";
+    daemon.settings = {
+      dns = [
+        "1.1.1.1"
+        "1.0.0.1"
+      ];
+    };
   };
   environment.systemPackages = with pkgs; [ docker ];
 }

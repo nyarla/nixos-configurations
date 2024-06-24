3034c0cf80923d870c7fb20f1026a9ee4c64f752
nyarla <nyarla@kalaclista.com>
2024-06-24 20:45:33 +0900

    chore(weston): remove workaround for weston

--- pkgs/temporary.nix
+++ pkgs/temporary.nix
@@ -1,9 +1,6 @@
 _: super:
-let
-  require = path: super.callPackage (import path);
-in
-{
-  weston = super.weston.override { vncSupport = false; };
+let require = path: super.callPackage (import path);
+in {
   whipper = super.whipper.overrideAttrs (_: rec {
     postPatch = ''
       sed -i 's|cd-paranoia|${super.cdparanoia}/bin/cdparanoia|g' whipper/program/cdparanoia.py

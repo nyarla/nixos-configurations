ffc38a9e9dbb5ff2d260df40cf157e5f1a8fba0f
nyarla <nyarla@kalaclista.com>
2024-08-18 08:13:28 +0900

    chore(lib/nixosSystem): add nix-wayland cache and public key to nix system

--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -45,10 +45,21 @@ let
                   inherit flake;
                 };
               };
-              settings.experimental-features = [
-                "nix-command"
-                "flakes"
-              ];
+              settings = {
+                trusted-public-keys = [
+                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                  "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+                ];
+                substituters = [
+                  "https://cache.nixos.org"
+                  "https://nixpkgs-wayland.cachix.org"
+                ];
+                experimental-features = [
+                  "nix-command"
+                  "flakes"
+                ];
+              };
+
               nixPath = [ "nixpkgs=${flake}" ];
             };
 

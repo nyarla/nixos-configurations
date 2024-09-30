a4c1760c72645f3a977907266496520f679b9fc5
nyarla <nyarla@kalaclista.com>
2024-09-30 14:17:44 +0900

    chore(firmware): enable to fwupd and gnome-firmware

--- system/config/hardware/firmware.nix
+++ system/config/hardware/firmware.nix
@@ -1,1 +1,6 @@
-_: { hardware.enableAllFirmware = true; }
+{ pkgs, ... }:
+{
+  hardware.enableAllFirmware = true;
+  services.fwupd.enable = true;
+  environment.systemPackages = with pkgs; [ gnome-firmware ];
+}

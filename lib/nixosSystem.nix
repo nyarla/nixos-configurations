1573361f4b2d9bf05c15eb40cefa0eb7fc0a3a83
nyarla <nyarla@kalaclista.com>
2024-10-11 12:31:50 +0900

    chore(nix): add `@wheel` to nix.settings.trusted-users

--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -51,10 +51,17 @@ let
                   "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
                   "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
                 ];
+
+                trusted-users = [
+                  "root"
+                  "@wheel"
+                ];
+
                 substituters = [
                   "https://cache.nixos.org"
                   "https://nixpkgs-wayland.cachix.org"
                 ];
+
                 experimental-features = [
                   "nix-command"
                   "flakes"

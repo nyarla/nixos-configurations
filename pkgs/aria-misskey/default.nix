b65b964f624282739ad4c35e9b54eea01c9a67d9
nyarla <nyarla@kalaclista.com>
2024-10-14 15:26:48 +0900

    chore(aria-misskey): update to v0.15.0

--- pkgs/aria-misskey/default.nix
+++ pkgs/aria-misskey/default.nix
@@ -13,8 +13,8 @@ let
   source = fetchFromGitHub {
     owner = "poppingmoon";
     repo = "aria";
-    rev = "a023375083b6d87238c5a0998bf82b27abb7a153";
-    hash = "sha256-fketb9ZIb+5wM31yeht19Vd4FInU8wskM2Z67Ffd6dg=";
+    rev = "30124fd8a9a74a21c92a1bc7eeebb1b39c42cd75";
+    hash = "sha256-54Mw/Ckh11El6+lUf/N64F0DskSM7b3gkiEZ60WF3Iw=";
     fetchSubmodules = true;
   };
 
@@ -42,8 +42,8 @@ flutter324.buildFlutterApplication rec {
 
   gitHashes = {
     "flutter_launcher_icons" = "sha256-/oYrlXShHgB/5u694Fe3PY7kCksaqgX59S3YaQf71Ik=";
-    "mfm_parser" = "sha256-mOvzB80Um8sk6jAhyvsm2uraYjrcwJ/dq0W6lZHDxWI=";
-    "misskey_dart" = "sha256-SC/J+knB3JjYJkQGLWazxqZqXzzVYESh981/dTo6f9E=";
+    "mfm_parser" = "sha256-zi/K0R9mGy4Y/eLyZYEP0eEQSftprDsjyA/++t1AJdY=";
+    "misskey_dart" = "sha256-WdQg21VpP5LbvQj8qMo9CBi2Wh3FREe8Wwlg54Xtyl0=";
     "tinycolor2" = "sha256-RGjhuX6487TJ2ofNdJS1FCMl6ciKU0qkGKg4yfkPE+w=";
     "twemoji_v2" = "sha256-Gi9PIMpw4NWkk357X+iZPUhzIKMDg5WdzTPHvJDBzSg=";
   };
@@ -52,7 +52,7 @@ flutter324.buildFlutterApplication rec {
   cargoDeps = rustPlatform.importCargoLock {
     lockFile = src + /rust/Cargo.lock;
     outputHashes = {
-      "aiscript-0.1.0" = "sha256-+I2WlUbaykjSmY9qKhR9SMeJ9cRMtSXO0Zw0Yyzjr1E=";
+      "aiscript-0.1.0" = "sha256-4OGaYbMlQn/Ejh8Ff2uhJbHTkf85CwoGOPdr9m5/B0U=";
     };
   };
 

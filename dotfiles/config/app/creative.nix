cc344d44533cf44b653abc5a8de2de481788a097
nyarla <nyarla@kalaclista.com>
2025-03-31 20:44:03 +0900

    chore(dotfiles/creative): add more DAW plugins

--- dotfiles/config/app/creative.nix
+++ dotfiles/config/app/creative.nix
@@ -47,27 +47,84 @@
       surge-XT
 
       # generator plugins
+      adlplug
+      aeolus
+      ams
       bespokesynth-with-vst2
+      bjumblr
       bristol
       chow-kick
       dexed
       drumgizmo
       drumkv1
+      geonkick
       helm
       hydrogen
+      librearp
+      ninjas2
       odin2
+      opnplug
+      oxefmsynth
+      padthv1
+      qsynth
+      samplv1
       sfizz
       sorcer
+      sorcer
+      synthv1
+      tunefish
       tunefish
       vital
+      x42-avldrums
+      x42-gmsynth
+      yoshimi
+      zynaddsubfx
 
       # effect plugins
       aether-lv2
       airwindows
       artyFX
+      bankstown-lv2
+      bchoppr
+      bolliedelayxt-lv2
+      boops
+      bs2b-lv2
+      bshapr
+      bslizr
       chow-tape-model
+      eq10q
+      fverb
+      gxplugins-lv2
+      kapitonov-plugins-pack
       lsp-plugins
+      lsp-plugins
+      mod-distortion
+      molot-lite
+      mooSpace
+      neural-amp-modeler-lv2
+      noise-repellent
+      rkrlv2
+      talentedhack
+      tap-plugins
+      vocproc
+      x42-plugins
       zam-plugins
+      calf
+
+      # others
+      bschaffl
+      bsequencer
+      calf
+      fomp
+      mda_lv2
+      midi-trigger
+      mod-arpeggiator-lv2
+      plujain-ramp
+      vcv-rack
+
+      # assets
+      aeolus-stops
+      freepats
     ]
     ++ [
       blender

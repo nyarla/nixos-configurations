1128b561e2bc4d5e89a95e7a35483a10a39a0c4e
nyarla <nyarla@kalaclista.com>
2025-05-13 13:24:02 +0900

    feat(NyZen9): use usb nvme storage as data drive

--- system/profile/NyZen9.nix
+++ system/profile/NyZen9.nix
@@ -259,6 +259,127 @@
   # Filesystem with Impermanence structure
   # --------------------------------------
 
+  # usb automount
+  systemd.services.automount-encrypted-usb-device = {
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+    description = "Automount service for encrypted usb storage";
+    path = with pkgs; [
+      coreutils
+      cryptsetup
+      "/run/wrappers/bin"
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+      ExecStart = toString (
+        pkgs.writeShellScript "mount.sh" ''
+          set -xeuo pipefail
+
+          export PATH=/run/wrappers/bin:$PATH
+
+          mount-if-exists() {
+            local deviceId
+            deviceId=$1
+
+            if [[ ! -e /dev/disk/by-uuid/$deviceId ]]; then
+              echo "/dev/disk/by-uuid/$deviceId is not found."
+              exit 0
+            fi
+
+            if [[ ! -e /boot/keys/$deviceId ]]; then
+              echo "/boot/keys/$deviceId is not found."
+              exit 0
+            fi
+
+            if [[ ! -e /persist/data/$deviceId ]]; then
+              mkdir -p /persist/data/$deviceId
+            fi
+
+            if [[ ! -e /dev/mapper/dev-disk-by-uuid-$deviceId ]]; then
+              cryptsetup luksOpen \
+                --key-file /boot/keys/$deviceId \
+                /dev/disk/by-uuid/$deviceId dev-disk-by-uuid-$deviceId
+            fi
+
+            if [[ ! -e /persist/data/$deviceId/.keep ]]; then
+              mount -t btrfs -o compress=zstd,ssd,space_cache=v2,x-gvfs-hide,noexec,nosuid,nodev \
+                /dev/mapper/dev-disk-by-uuid-$deviceId \
+                /persist/data/$deviceId
+            fi
+
+            return 0
+          }
+
+          backup-if-exists() {
+            local deviceId
+            deviceId=$1
+
+            local dirName
+            dirName=$2
+
+            if [[ ! -e /persist/data/$deviceId/$dirName ]]; then
+              echo "/persist/data/$deviceId/$dirName is not found."
+              exit 0
+            fi
+
+            if [[ ! -e /backup/$dirName ]]; then
+              mkdir -p /backup/$dirName
+            fi
+
+            mount -o bind /persist/data/$deviceId/$dirName /backup/$dirName
+            return $?
+          }
+
+          mount-if-exists 14887bd8-3e3c-4675-9e81-9027a050cdf7
+          backup-if-exists 14887bd8-3e3c-4675-9e81-9027a050cdf7 Calibre
+          backup-if-exists 14887bd8-3e3c-4675-9e81-9027a050cdf7 Music
+        ''
+      );
+
+      ExecStop = toString (
+        pkgs.writeShellScript "unmount.sh" ''
+          set -xeuo pipefail
+
+          export PATH=/run/wrappers/bin:$PATH
+
+          unbind-if-exists() {
+            local dirName
+            dirName=$1
+
+            if [[ -e /backup/$dirName ]]; then
+              umount /backup/$dirName
+            fi
+
+            return 0
+          }
+
+          unmount-if-exists() {
+            local deviceId
+            deviceId=$1
+
+            if [[ -e /persist/data/$deviceId ]]; then
+              umount /persist/data/$deviceId || true
+            fi
+
+            if [[ -e /dev/mapper/dev-disk-by-uuid-$deviceId ]]; then
+              cryptsetup luksClose /dev/mapper/dev-disk-by-uuid-$deviceId
+            fi
+
+            return 0
+          }
+
+          unbind-if-exists Calibre
+          unbind-if-exists Music
+
+          unmount-if-exists 14887bd8-3e3c-4675-9e81-9027a050cdf7
+
+          exit 0
+        ''
+      );
+    };
+  };
+
   # mount
   fileSystems =
     let
@@ -400,20 +521,12 @@
         dest = "/persist/home/nyarla/Archives";
       }
       {
-        name = "Calibre";
-        dest = "/persist/home/nyarla/Calibre";
-      }
-      {
         name = "Development";
         dest = "/persist/home/nyarla/Development";
       }
       {
         name = "Documents";
         dest = "/persist/home/nyarla/Documents";
-      }
-      {
-        name = "Music";
-        dest = "/persist/home/nyarla/Music";
       }
       {
         name = "NixOS";
@@ -445,6 +558,9 @@
   services.btrfs.autoScrub.enable = true;
   services.btrfs.autoScrub.fileSystems = [
     "/nix"
+
+    "/persist/data/14887bd8-3e3c-4675-9e81-9027a050cdf7/Calibre"
+    "/persist/data/14887bd8-3e3c-4675-9e81-9027a050cdf7/Music"
 
     "/persist/etc"
     "/persist/etc/nixos"
@@ -636,6 +752,9 @@
       snapshots = paths: lib.attrsets.concatMapAttrs (n: v: { "${n}" = snapshot v; }) paths;
     in
     snapshots {
+      data-music = "/persist/data/14887bd8-3e3c-4675-9e81-9027a050cdf7/Music";
+      data-calibre = "/persist/data/14887bd8-3e3c-4675-9e81-9027a050cdf7/Music";
+
       etc-nixos = "/persist/etc/nixos";
 
       home-nyarla = "/persist/home/nyarla";

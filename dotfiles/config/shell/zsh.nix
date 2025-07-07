1d0a19e1715126bb58d6df85ef460eaf089a2143
nyarla <nyarla@kalaclista.com>
2025-07-07 11:43:34 +0900

    refactor(zsh): rewrite zsh configurations

--- dotfiles/config/shell/zsh.nix
+++ dotfiles/config/shell/zsh.nix
@@ -2,26 +2,106 @@
 {
   programs.zsh = {
     enable = true;
-    autocd = true;
-    shellAliases = {
-      "docker" = "env DBUS_SESSION_BUS_ADDRESS= docker";
-      "l" = "ls --color -F -la";
-      "ls" = "ls --color -F";
-      "nixos-apply" =
-        ''sudo nixos-rebuild switch --flake "/etc/nixos#$(hostname)" ; sudo systemctl restart home-manager-$(id -n -u).service'';
-      "nixos-build" = ''sudo nixos-rebuild build --flake "/etc/nixos/#$(hostname)"'';
-      "nixos-upgrade" = ''sudo nixos-rebuild boot --flake "/etc/nixos#$(hostname)"'';
-      "podman" = "env DBUS_SESSION_BUS_ADDRESS= podman";
-      "waydroid" = "env XDG_DATA_HOME=/persist/home/nyarla/.local/share waydroid";
-    };
-    sessionVariables = {
-      FAKE_RELEASE = 1;
-      GOPATH = "$HOME/Applications/Development/go";
-      NIXPKGS_ALLOW_UNFREE = 1;
-      EDITOR = "nvim";
-    };
+
+    # completion
     enableCompletion = true;
+    autocd = true;
+
+    # highlight
     syntaxHighlighting.enable = true;
+
+    # zshrc
+    initContent =
+      let
+        PATH = lib.strings.concatStringsSep ":" [
+          "$HOME/.nix-profile/bin"
+          "$HOME/.local/bin"
+          "$GOPATH/bin"
+          "$PATH"
+        ];
+
+        options = lib.mkOrder 500 ''
+          export PATH=${PATH}
+
+          setopt auto_pushd
+          setopt pushd_ignore_dups
+          setopt pushd_to_home
+
+          setopt hist_ignore_all_dups
+          setopt hist_reduce_blanks
+          setopt hist_find_no_dups
+        '';
+
+        functions = lib.mkOrder 551 ''
+          function has() {
+            type "''${1:-}" >/dev/null 2>&1
+          }
+
+          function nixos-clean() {
+            sudo nix-collect-garbage --delete-older-than 7d
+            sudo /run/current-system/bin/switch-to-configuration boot
+          }
+
+          if has fzy ; then
+            function __bind-history() {
+              BUFFER="$(fc -l -n 1 | sort -u | fzy --query "''${LBUFFER}")"
+              CURSOR=$#BUFFER
+              zle -R -c
+            }
+
+            zle -N __bind-history
+            bindkey '^R' __bind-history 
+          fi
+
+          if has wl-paste ; then
+            function __bind-clipboard() {
+              BUFFER=$(wl-paste)
+              CURSOR=$#BUFFER
+              zle -R -c
+            }
+
+            zle -N __bind-clipboard
+            bindkey '^P' __bind-clipboard
+          fi
+        '';
+      in
+      lib.mkMerge [
+        options
+        functions
+      ];
+
+    # aliases
+    shellAliases =
+      let
+        podman = "env DBUS_SESSION_BUS_ADDRESS= podman";
+        ls = "ls --color -F";
+        nixos-rebuild = action: "sudo nixos-rebuild ${action} --flake /etc/nixos#$(hostname)";
+      in
+      {
+        inherit ls;
+        l = "${ls} -l";
+
+        docker = podman;
+        inherit podman;
+
+        nixos-apply = "${nixos-rebuild "apply"} && sudo systemctl restart home-manager-$(id -n -u).service";
+        nixos-build = nixos-rebuild "build";
+        nixos-upgrade = nixos-rebuild "boot";
+
+        waydroid = "env env XDG_DATA_HOME=/persist/home/nyarla/.local/share waydroid";
+      };
+    sessionVariables = {
+      # editor
+      EDITOR = "nvim";
+
+      # development
+      GOPATH = "$HOME/Applications/Development/go";
+      FAKE_RELEASE = 1;
+
+      # nixpkgs
+      NIXPKGS_ALLOW_UNFREE = 1;
+    };
+
     history = {
       size = 10000;
       path = "$HOME/.zsh_history";
@@ -29,6 +109,7 @@
       ignoreDups = true;
       share = true;
     };
+
     plugins = [
       {
         name = "enhancd";
@@ -41,60 +122,6 @@
         };
       }
     ];
-    initExtraFirst = ''
-      # enable home-manager
-      export PATH=${
-        lib.strings.concatStringsSep ":" [
-          "$HOME/.nix-profile/bin"
-          "$HOME/.local/bin"
-          "$HOME/.fly/bin"
-          "$GOPATH/bin"
-          "$PATH"
-        ]
-      }
-
-      # utility function
-      function has() {
-        type "''${1:-}" >/dev/null 2>&1
-      }
-    '';
-    initExtra = ''
-      setopt auto_pushd
-      setopt pushd_ignore_dups
-      setopt pushd_to_home
-      setopt hist_ignore_all_dups
-      setopt hist_reduce_blanks
-      setopt hist_find_no_dups
-
-      function nixos-clean() {
-        sudo nix-collect-garbage --delete-older-than 7d
-        sudo /run/current-system/bin/switch-to-configuration boot
-      }
-
-      if has fzy ; then
-        function fzy-history() {
-          BUFFER="$(fc -l -n 1 | sort -u | fzy --query "''${LBUFFER}")"
-          CURSOR=$#BUFFER
-          zle -R -c
-        }
-
-        zle -N fzy-history
-        bindkey '^R' fzy-history
-      fi
-
-      if has wl-paste ; then
-        function paste-clipboard() {
-          BUFFER=$(wl-paste)
-          CURSOR=$#BUFFER
-          zle -R -c
-        }
-
-        zle -N paste-clipboard
-        bindkey '^P' paste-clipboard
-      fi
-
-      unset -f has
-    '';
   };
 
   home.packages = with pkgs; [

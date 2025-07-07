68aacebb1e9514aefd9da79fd290fad7ea9a627e
nyarla <nyarla@kalaclista.com>
2025-07-07 12:47:36 +0900

    refactor(i18n): update glibc locales

--- pkgs/glibc-locales-eaw/default.nix
+++ pkgs/glibc-locales-eaw/default.nix
@@ -7,8 +7,8 @@
 
 let
   UTF-8-EAW-CONSOLE = fetchurl {
-    url = "https://github.com/hamano/locale-eaw/raw/7623d45f9b87d9b44dca6ef8c46fc97b22690f50/dist/UTF-8-EAW-CONSOLE";
-    sha256 = "1jm54m7abyc0fvvavr2qilrbiilcp8fz5k9d4fp6pdqpq87k9irv";
+    url = "https://raw.githubusercontent.com/hamano/locale-eaw/6f8d5ddb29da3bde73e3d617ece5ba9fd16c335c/dist/UTF-8-EAW-CONSOLE";
+    sha256 = "1d2zmkvaydwf0ra22gg9fms90ja597mxvbd1b05k26mkd91ck675";
   };
 in
 
--- system/config/i18n/locales.nix
+++ system/config/i18n/locales.nix
@@ -2,13 +2,13 @@
 {
   i18n = rec {
     defaultLocale = "en_US.UTF-8";
-    supportedLocales = [
+    extraLocales = [
       "en_US.UTF-8/UTF-8-EAW-CONSOLE"
       "ja_JP.UTF-8/UTF-8-EAW-CONSOLE"
     ];
     glibcLocales = pkgs.glibc-locales-eaw.override {
       allLocales = false;
-      locales = supportedLocales;
+      locales = extraLocales;
     };
   };
 }

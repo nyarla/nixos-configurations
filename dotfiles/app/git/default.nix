2ad4718f071a65e6e492c340fe17aa5b9b73aa3a
nyarla <nyarla@kalaclista.com>
2025-07-07 11:44:29 +0900

    refactor(dotfiles/git): refactor to command aliases

--- dotfiles/app/git/default.nix
+++ dotfiles/app/git/default.nix
@@ -5,11 +5,13 @@
     lfs.enable = true;
     package = pkgs.gitFull;
     aliases = {
-      co = "checkout";
       ci = "commit";
-      force-push = "push -f --force-with-lease --force-if-includes";
-      st = "status";
       cleanup = "!git branch --merged | grep -v main | grep -v master | xargs -I{} git branch -d {}";
+      co = "checkout";
+      force-push = "push -f --force-with-lease --force-if-includes";
+      rr = "restore";
+      rs = "restore --staged";
+      st = "status";
     };
     extraConfig = {
       color = {

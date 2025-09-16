5e34c4469ff1bed30f02d448d47b19ce2ea7c7cb
nyarla <nyarla@kalaclista.com>
2025-09-17 08:46:39 +0900

    refactor(theme): use `lib/theme.nix` file for colorscheme

--- dotfiles/app/mlterm/color.nix
+++ dotfiles/app/mlterm/color.nix
@@ -1,19 +1,25 @@
-{
-  black = "#111111"; # 5
-  red = "#f77468"; # 65
-  green = "#aee46f"; # 85
-  yellow = "#ffcb61"; # 85
-  blue = "#8cc8ff"; # 85
-  magenta = "#fb7fd5"; # 85
-  cyan = "#00efff"; # 85
-  white = "#f1f1f1"; # 95
+let
+  theme = import ../../../lib/theme.nix;
+in
 
-  hl_black = "#9e9e9e"; # 65
-  hl_red = "#ff8274"; # 75
-  hl_green = "#bcf27c"; # 90
-  hl_yellow = "#ffd96e"; # 90
-  hl_blue = "#abe4ff"; # 90
-  hl_magenta = "#ffb7ff"; # 90
-  hl_cyan = "#00ffff"; # 95
-  hl_white = "#ffffff"; # 100
+with theme;
+
+{
+  black = gray05;
+  red = red65;
+  green = green85;
+  yellow = yellow85;
+  blue = blue85;
+  magenta = magenta95;
+  cyan = cyan85;
+  white = gray95;
+
+  hl_black = gray65;
+  hl_red = red75;
+  hl_green = green90;
+  hl_yellow = yellow90;
+  hl_blue = blue90;
+  hl_magenta = magenta90;
+  hl_cyan = cyan90;
+  hl_white = gray100;
 }
--- system/config/linux/console.nix
+++ system/config/linux/console.nix
@@ -1,23 +1,28 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  theme = import ../../../lib/theme.nix;
+in
 {
-  console.colors = [
-    "000000"
-    "FF6633"
-    "CCFF00"
-    "FFCC33"
-    "00CCFF"
-    "CC99CC"
-    "00CCCC"
-    "F9F9F9"
-    "666666"
-    "FF6633"
-    "CCFF00"
-    "FFCC33"
-    "00CCFF"
-    "CC99CC"
-    "00CCCC"
-    "FFFFFF"
-  ];
+  console.colors =
+    with theme;
+    lib.lists.forEach [
+      gray00
+      red65
+      green85
+      yellow85
+      blue85
+      magenta95
+      cyan85
+      gray95
+      gray65
+      red75
+      green90
+      yellow90
+      blue90
+      magenta90
+      cyan95
+      gray100
+    ] (x: lib.strings.replaceString "#" "" x);
 
   console.earlySetup = true;
 }

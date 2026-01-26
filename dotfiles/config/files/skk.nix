c45cd950f226e757fe92e43a3b5cdd8a5501284c
nyarla <nyarla@kalaclista.com>
2026-01-26 19:57:02 +0900

    chore(dotfiles/skk): fix typo to symlink of SKK-JISYO.XL

--- dotfiles/config/files/skk.nix
+++ dotfiles/config/files/skk.nix
@@ -1,4 +1,4 @@
 { pkgs, ... }:
 {
-  xdg.configFile."skk/SKK-JISYO.XL".source = "${pkgs.skk-dicts-xl}/share/skk/SkK-JISYO.XL";
+  xdg.configFile."skk/SKK-JISYO.XL".source = "${pkgs.skk-dicts-xl}/share/skk/SKK-JISYO.XL";
 }

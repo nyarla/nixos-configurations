333d28228dd5025cc7121c3ca84ea6c56e348b53
nyarla <nyarla@kalaclista.com>
2026-01-31 15:17:13 +0900

    refactor(NyZen9/vr): launch WiVRN by manually

--- dotfiles/config/app/vr.nix
+++ dotfiles/config/app/vr.nix
@@ -2,22 +2,35 @@
 {
   home.packages = with pkgs; [
     immersed
+    wayvr
+    wivrn-stable
   ];
 
-  xdg.configFile."openvr/openvrpaths.vrpath".text = ''
-    {
-      "config": [
-        "${config.xdg.dataHome}/Steam/config"
-      ],
-      "external_drivers": null,
-      "jsonid": "vrpathreg",
-      "log": [
-        "${config.xdg.dataHome}/Steam/logs"
-      ],
-      "runtime": [
-        "${pkgs.xrizer}/lib/xrizer"
-      ],
-      "version": 1
-    }
-  '';
+  xdg.configFile."openvr/openvrpaths.vrpath".text = builtins.toJSON {
+    version = 1;
+    config = [
+      "${config.xdg.dataHome}/Steam/config"
+    ];
+    external_drivers = null;
+    jsonid = "vrpathreg";
+    log = [
+      "${config.xdg.dataHome}/Steam/logs"
+    ];
+    runtime = [
+      "${pkgs.xrizer}/lib/xrizer"
+    ];
+  };
+
+  xdg.configFile."wivrn/config.json".text = builtins.toJSON {
+    bitrate = 50000000;
+    scale = 1;
+    tcp-only = true;
+    encoders = [
+      {
+        encoder = "vaapi";
+        codec = "h265";
+      }
+    ];
+    openvr-compat-path = "${pkgs.xrizer}/lib/xrizer";
+  };
 }
--- system/config/gaming/xr.nix
+++ system/config/gaming/xr.nix
@@ -1,45 +1,8 @@
-{ pkgs, ... }:
-{
+_: {
   nixpkgs.xr.enable = true;
 
   # for ntsync
   boot.kernelModules = [
     "ntsync"
-  ];
-
-  # wivrn
-  services.wivrn = {
-    package = pkgs.wivrn-stable;
-    enable = true;
-    autoStart = true;
-    defaultRuntime = true;
-    highPriority = true;
-    monadoEnvironment = {
-      DRI_PRIME = "1002:7550!";
-      MESA_VK_DEVICE_SELECT = "1002:7550";
-      AMD_VULKAN_ICD = "RADV";
-      VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
-
-      WMR_HANDTRACKING = "1";
-    };
-    config = {
-      enable = true;
-      json = {
-        bitrate = 50000000;
-        scale = 1;
-        tcp-only = true;
-        encoders = [
-          {
-            encoder = "vaapi";
-            codec = "h265";
-          }
-        ];
-        openvr-compat-path = "${pkgs.xrizer}/lib/xrizer";
-      };
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    wayvr
   ];
 }

9d376e5693f19cd2ddb1c94c27dd3cf52222fb50
nyarla <nyarla@kalaclista.com>
2026-03-12 11:53:36 +0900

    chore(dotfiles/vr): use WiRVn as openxr runtime

--- dotfiles/config/app/vr.nix
+++ dotfiles/config/app/vr.nix
@@ -6,6 +6,9 @@
     wivrn-stable
   ];
 
+  xdg.configFile."openxr/1/active_runtime.json".source =
+    "${pkgs.wivrn-stable}/share/openxr/1/openxr_wivrn.json";
+
   xdg.configFile."openvr/openvrpaths.vrpath".text = builtins.toJSON {
     version = 1;
     config = [

a9cc4eb457f3a93b04268b7b19c97b04b4bfc1af
nyarla <nyarla@kalaclista.com>
2026-06-05 14:32:44 +0900

    feat(NyZen9/services): add Open WebUI and ComfyUI services

--- system/config/services/nyzen9.nix
+++ system/config/services/nyzen9.nix
@@ -19,6 +19,95 @@
     };
   };
 
+  # Open WebUI
+  services.open-webui = {
+    enable = true;
+    stateDir = "/persist/home/nyarla/Applications/AI/NixOSUI.Data";
+    port = 21080;
+    environment = {
+      ENABLE_SIGNUP = "False";
+      WEBUI_ADMIN_NAME = "nyarla";
+      WEBUI_ADMIN_EMAIL = "me@nyarla.jp";
+      WEBUI_ADMIN_PASSWORD = "init12345!@#$%";
+      DEFAULT_LOCALE = "ja";
+
+      ENABLE_WEB_SEARCH = "True";
+      WEB_SEARCH_ENGINE = "duckduckgo";
+    };
+  };
+
+  systemd.services.open-webui.serviceConfig = {
+    DynamicUser = lib.mkForce false;
+    PrivateUSer = lib.mkForce false;
+    User = "nyarla";
+    Group = "users";
+  };
+
+  services.nginx.virtualHosts."llm.services.thotep.net" = {
+    useACMEHost = "services.thotep.net";
+    forceSSL = true;
+    listen =
+      let
+        addr = "100.103.65.77";
+      in
+      [
+        {
+          inherit addr;
+          port = 80;
+          ssl = false;
+        }
+        {
+          inherit addr;
+          port = 443;
+          ssl = true;
+        }
+      ];
+
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:21080";
+      proxyWebsockets = true;
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $remote_addr;
+        proxy_set_header X-Forwarded-Proto $scheme;
+      '';
+    };
+  };
+
+  services.nginx.virtualHosts."comfyui.services.thotep.net" = {
+    useACMEHost = "services.thotep.net";
+    forceSSL = true;
+    listen =
+      let
+        addr = "100.103.65.77";
+      in
+      [
+        {
+          inherit addr;
+          port = 80;
+          ssl = false;
+        }
+        {
+          inherit addr;
+          port = 443;
+          ssl = true;
+        }
+      ];
+
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:8188";
+      proxyWebsockets = true;
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $remote_addr;
+        proxy_set_header X-Forwarded-Proto $scheme;
+      '';
+    };
+
+  };
+
   # FreshRSS for localhost
   services.freshrss = {
     enable = true;

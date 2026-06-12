6ccfc7093d7b739915de80f17222a7bedcea2c7c
nyarla <nyarla@kalaclista.com>
2026-06-12 14:25:11 +0900

    chore(pkgs): update glibc-locales-eaw

--- pkgs/glibc-locales-eaw/default.nix
+++ pkgs/glibc-locales-eaw/default.nix
@@ -7,8 +7,8 @@
 
 let
   UTF-8-EAW-CONSOLE = fetchurl {
-    url = "https://raw.githubusercontent.com/hamano/locale-eaw/6f8d5ddb29da3bde73e3d617ece5ba9fd16c335c/dist/UTF-8-EAW-CONSOLE";
-    sha256 = "1d2zmkvaydwf0ra22gg9fms90ja597mxvbd1b05k26mkd91ck675";
+    url = "https://raw.githubusercontent.com/hamano/locale-eaw/257ec45a95e377a397483a29f524c6648d4fe925/dist/UTF-8-EAW-CONSOLE";
+    sha256 = "0rnwgav20ijr5zvbvy9wix5isq2gw4fp6m89ijyszfy337aqb7s5";
   };
 in
 
@@ -19,6 +19,12 @@ in
   (old: {
     preBuild = ''
       cp ${UTF-8-EAW-CONSOLE} ../localedata/charmaps/UTF-8-EAW-CONSOLE
+      chmod +w ../localedata/charmaps/UTF-8-EAW-CONSOLE
+
+      substituteInPlace ../localedata/charmaps/UTF-8-EAW-CONSOLE \
+        --replace-fail "<U2190>...<U21FF> 2" "<U2190>...<U21FF> 1" \
+        --replace-fail "<U0001F100>...<U0001F77F> 2" "<U0001F100>...<U0001F1E5> 2\n<U0001F200>...<U0001F77F> 2"
+
       echo 'ja_JP.UTF-8/UTF-8-EAW-CONSOLE \' >> ../localedata/SUPPORTED
       echo 'en_US.UTF-8/UTF-8-EAW-CONSOLE \' >> ../localedata/SUPPORTED
     ''
